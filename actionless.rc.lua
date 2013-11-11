cd01fbf0b9cf7bd1400ca8d5f05dc4c41b0d0cab
actionless <actionless.loveless@gmail.com>
2013-11-11 16:27:41 +0300

    changed widget order on awesome panel

--- actionless.rc.lua
+++ actionless.rc.lua
@@ -473,14 +473,15 @@ for s = 1, screen.count() do
 
 	-- Widgets that are aligned to the right
 	local right_layout = wibox.layout.fixed.horizontal()
-	if s == 1 then right_layout:add(wibox.widget.systray()) end
 	right_layout:add(spr)
+	right_layout:add(arrl)
+	right_layout:add(mpdicon)
+	right_layout:add(mpdwidgetbg)
 	right_layout:add(arrl)
 	right_layout:add(voliconbg)
 	right_layout:add(volumewidgetbg)
 	right_layout:add(arrl)
-	right_layout:add(mpdicon)
-	right_layout:add(mpdwidgetbg)
+	if s == 1 then right_layout:add(wibox.widget.systray()) end
 	right_layout:add(arrl)
 	right_layout:add(memicon)
 	right_layout:add(memwidget)
