1b9f021157cf7111af36b26a59d72285bbdc37e0
actionless <actionless.loveless@gmail.com>
2014-02-17 22:50:47 +0300

    started to monkeycode battery widget

--- toolbar.lua
+++ toolbar.lua
@@ -100,22 +100,29 @@ mytextclock = awful.widget.textclock(" %H:%M")
 widgets.calendar:attach(mytextclock)
 
 -- Battery
--- baticon = wibox.widget.imagebox(beautiful.widget_battery)
--- batwidget = lain.widgets.bat({
---	 settings = function()
---		 if bat_now.perc == "N/A" then
---			 bat_now.perc = "AC"
---			 baticon:set_image(beautiful.widget_ac)
---		elseif tonumber(bat_now.perc) <= 5 then
---			 baticon:set_image(beautiful.widget_battery_empty)
---		 elseif tonumber(bat_now.perc) <= 15 then
---			 baticon:set_image(beautiful.widget_battery_low)
---		 else
---			 baticon:set_image(beautiful.widget_battery)
---		 end
---		 widget:set_markup(" " .. bat_now.perc .. " ")
---	 end
---})
+baticon = wibox.widget.imagebox(beautiful.widget_battery)
+batwidget = widgets.bat({
+	 settings = function()
+		if bat_now.on_bat == "no" then
+			baticon:set_image(beautiful.widget_ac)
+		 	widget:set_bg(beautiful.bg)
+		 	widget:set_fg(beautiful.fg)
+		elseif bat_now.lo_bat == 'yes' then
+			baticon:set_image(beautiful.widget_battery_empty)
+		 	widget:set_bg(beautiful.error)
+		 	widget:set_fg(beautiful.bg)
+		elseif tonumber(bat_now.perc) <= 25 then
+			baticon:set_image(beautiful.widget_battery_low)
+		 	widget:set_bg(beautiful.fg)
+		 	widget:set_fg(beautiful.bg)
+		else
+			baticon:set_image(beautiful.widget_battery)
+		 	widget:set_bg(beautiful.bg)
+		 	widget:set_fg(beautiful.fg)
+		end
+		widget.widget:set_markup(string.format("%-4s",bat_now.perc .. "%"))
+	end
+})
 
 -- Separators
 spr = wibox.widget.textbox(' ')
@@ -224,8 +231,8 @@ for s = 1, screen.count() do
 	--right_layout:add(fsicon)
 	--right_layout:add(fswidgetbg)
 	--right_layout:add(arrl)
-	-- right_layout:add(baticon)
-	-- right_layout:add(batwidget)
+	right_layout:add(baticon)
+	right_layout:add(batwidget)
 	right_layout:add(mytextclock)
 	right_layout:add(spr)
 --	right_layout:add(arrl_ld)
---
+++ widgets/bat.lua
@@ -0,0 +1,85 @@
+
+--[[
+                                                  
+     Licensed under GNU General Public License v2 
+      * (c) 2013,      Luke Bonham                
+      * (c) 2010-2012, Peter Hofmann              
+                                                  
+--]]
+
+local asyncshell   = require("widgets.asyncshell")
+local newtimer     = require("lain.helpers").newtimer
+local first_line   = require("lain.helpers").first_line
+
+local naughty      = require("naughty")
+local wibox        = require("wibox")
+
+local math         = { floor  = math.floor }
+local string       = { format = string.format,
+						match = string.match }
+
+local setmetatable = setmetatable
+
+-- Battery infos
+-- lain.widgets.bat
+local bat = {}
+bat_now = {
+	on_bat = "N/A",
+	lo_bat = "N/A",
+	perc   = "N/A",
+	time   = "N/A",
+}
+bat_prev = nil
+
+local function worker(args)
+    local args = args or {}
+    local timeout = args.timeout or 30
+    local battery = args.battery or "BAT0"
+    local settings = args.settings or function() end
+
+    bat.widget_text = wibox.widget.textbox('')
+    bat.widget = wibox.widget.background()
+	bat.widget:set_widget(bat.widget_text)
+
+	function update()
+		asyncshell.request('upower -d', function(f) post_update_battery(f) end)
+	end
+
+    function post_update_battery(f)
+        for line in f:lines() do 
+			k, v = string.match(line, "[ ]+(.*):[ ]+(.*)")
+			if k == 'percentage' then
+				bat_now.perc = string.match(v,"%d+")
+			elseif k == 'time to empty' then
+				bat_now.time = tonumber(v)
+			elseif k == 'on-battery' then
+				bat_now.on_bat = v
+			elseif k == 'on-low-battery' then
+				bat_now.lo_bat = v
+			end 
+		end 
+		-- notifications for low and critical states
+		--if bat_now.perc <= 5
+		--then
+        --        bat.id = naughty.notify({
+        --            text = "shutdown imminent",
+        --            title = "battery nearly exhausted",
+        --            position = "top_right",
+        --            timeout = 15,
+        --            fg="#000000",
+        --            bg="#ffffff",
+        --            ontop = true,
+        --            replaces_id = bat.id
+        --        }).id
+		--end
+        widget = bat.widget
+        settings()
+		bat_prev = bat_now
+    end
+
+    newtimer("bat_widget", timeout, update)
+
+    return bat.widget
+end
+
+return setmetatable(bat, { __call = function(_, ...) return worker(...) end })
