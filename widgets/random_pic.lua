500d06dfd7a61706f016066c5a1b986b7fa2bb37
actionless <actionless.loveless@gmail.com>
2014-05-16 15:04:08 +0200

    awesome: widgets: random_pic: small refactoring

--- widgets/random_pic.lua
+++ widgets/random_pic.lua
@@ -1,18 +1,14 @@
-
 --[[
-
      Licensed under GNU General Public License v2
       * (c) 2013-2014, Yauheni Kirylau
-      * (c) 2013,      Luke Bonham
-
 --]]
+
 local asyncshell   = require("widgets.asyncshell")
 local newtimer     = require("widgets.helpers").newtimer
 local icons_dir     = require("widgets.helpers").icons_dir
 
 local wibox        = require("wibox")
 
-local io           = io
 local tonumber     = tonumber
 local math         = require("math")
 
@@ -20,7 +16,7 @@ local setmetatable = setmetatable
 local beautiful    = require("widgets.helpers").beautiful
 
 
-function scandir(directory)
+local function scandir(directory)
     local i, t, popen = 0, {}, io.popen
 	result = {}
     for filename in popen('ls -a "'..directory..'"'):lines() do
@@ -32,30 +28,32 @@ function scandir(directory)
     return result
 end
 
+local rp = {}
+rp.widget = wibox.widget.imagebox()
 
 local function worker(args)
-	local rp = {}
 	local args     = args or {}
+
 	--local interval  = args.interval or 5
 	local interval  = args.interval or math.random(5,15)
-	local dir = args.dir or icons_dir .. 'random_pics/'
 
+	local dir = args.dir or icons_dir .. 'random_pics/'
 	local image_list = scandir(dir)
+	rp.widget:set_image(random_image())
 
 	function random_image()
 		local image_name = image_list[ math.random(1, #image_list) ]
 		return dir .. image_name
 	end
-
-
-	rp.widget = wibox.widget.imagebox()
-	rp.widget:set_image(random_image())
 	
-	function update()
+	function rp.update()
 		rp.widget:set_image(random_image())
 	end
+	
+	-- random timer id is for possibility to
+	-- run different instance of the widget simultaneously
+	newtimer("random_pic_" .. math.random(1,65535), interval, rp.update)
 
-	newtimer("random_pic_" .. math.random(1,65535), interval, update)
 	return rp.widget
 end
 
