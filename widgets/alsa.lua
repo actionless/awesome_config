2bf3761120cb5387566ab874bdc9cee4e980e4a4
actionless <actionless.loveless@gmail.com>
2014-05-19 17:24:20 +0200

    code style: widgets: alsa: applied formatter

--- widgets/alsa.lua
+++ widgets/alsa.lua
@@ -1,19 +1,16 @@
-
 --[[
-												  
-	 Licensed under GNU General Public License v2 
-	  * (c) 2013-2014, Yauhen Kirylau
-	  * (c) 2013, Luke Bonham					 
-	  * (c) 2010, Adrian C. <anrxc@sysphere.org>  
-												  
+  Licensed under GNU General Public License v2 
+   * (c) 2013-2014, Yauhen Kirylau
+   * (c) 2013, Luke Bonham
+   * (c) 2010, Adrian C. <anrxc@sysphere.org>  
 --]]
 
-local wibox		   = require("wibox")
-local awful		   = require("awful")
+local wibox		= require("wibox")
+local awful		= require("awful")
 
-local io			  = { popen  = io.popen }
-local string		  = { match  = string.match,
-                          format = string.format }
+local io		= { popen  = io.popen }
+local string		= { match  = string.match,
+                            format = string.format }
 local setmetatable	= setmetatable
 
 local asyncshell	= require("widgets.asyncshell")
@@ -26,139 +23,131 @@ local beautiful		= helpers.beautiful
 local alsa = {}
 alsa.widget = common.widget(beautiful.widget_vol)
 alsa.widget:buttons(awful.util.table.join(
-	awful.button({ }, 1, function () alsa.toggle() end),
-	awful.button({ }, 5, function () alsa.down() end),
-	awful.button({ }, 4, function () alsa.up() end)
+  awful.button({ }, 1, function () alsa.toggle() end),
+  awful.button({ }, 5, function () alsa.down() end),
+  awful.button({ }, 4, function () alsa.up() end)
 ))
 
 alsa.volume = {
-	status = "N/A",
-	level = "0"
+  status = "N/A",
+  level = "0"
 }
 
 local function worker(args)
-	local args	 = args or {}
-	local with_icon = args.with_icon or true
-	local widget_bg = args.widget_bg
-        alsa.step = args.step or 2
-	alsa.timeout  = args.timeout or 5
-	alsa.channel  = args.channel or "Master"
-	alsa.mic_channel = args.mic_channel or "Capture"
-	alsa.channels_toggle = args.channels_toggle or {channel, }
+  local args = args or {}
+  alsa.step = args.step or 2
+  alsa.timeout  = args.timeout or 5
+  alsa.channel  = args.channel or "Master"
+  alsa.mic_channel = args.mic_channel or "Capture"
+  alsa.channels_toggle = args.channels_toggle or {channel, }
 
-        helpers.set_map("volume in progress", false)
+  helpers.set_map("volume in progress", false)
 
-	if widget_bg then
-		alsa.icon_bg:set_bg(widget_bg)
-		alsa.text_bg:set_bg(widget_bg)
-	end
+  function alsa.up()
+    if helpers.get_map("volume in progress") then
+      return
+    end
+    helpers.set_map("volume in progress", true)
 
-	function alsa.up()
-                if helpers.get_map("volume in progress") then
-                  return
-                end
-                helpers.set_map("volume in progress", true)
-		if alsa.volume.level < 100 then
-			alsa.volume.level = alsa.volume.level + alsa.step
-		end
-		asyncshell.request(
-                  "amixer -q set " .. alsa.channel .. ",0 " .. alsa.step .. "%+",
-		  function(f) alsa.post_volume() end)
-	end
+    if alsa.volume.level < 100 then
+      alsa.volume.level = alsa.volume.level + alsa.step
+    end
+    asyncshell.request(
+      "amixer -q set " .. alsa.channel .. ",0 " .. alsa.step .. "%+",
+      function(f) alsa.post_volume() end)
+  end
 
-	function alsa.down()
-                if helpers.get_map("volume in progress") then
-                  return
-                end
-                helpers.set_map("volume in progress", true)
-		if alsa.volume.level > 0 then
-			alsa.volume.level = alsa.volume.level - alsa.step
-		end
-		asyncshell.request(
-                  "amixer -q set " .. alsa.channel .. ",0 " .. alsa.step .. "%-",
-		  function(f) alsa.post_volume() end)
-	end
+  function alsa.down()
+    if helpers.get_map("volume in progress") then
+      return
+    end
+    helpers.set_map("volume in progress", true)
 
-        function alsa.post_volume()
-          helpers.set_map("volume in progress", false)
-	  alsa.update_indicator()
-        end
+    if alsa.volume.level > 0 then
+      alsa.volume.level = alsa.volume.level - alsa.step
+    end
+    asyncshell.request(
+      "amixer -q set " .. alsa.channel .. ",0 " .. alsa.step .. "%-",
+      function(f) alsa.post_volume() end)
+  end
 
-	function alsa.toggle()
-		if alsa.volume.status == 'off' then
-			for _, channel in pairs(alsa.channels_toggle) do 
-				awful.util.spawn_with_shell("amixer -q set " .. channel .. ",0 on")
-				awful.util.spawn_with_shell("amixer -q set " .. channel .. ",1 on")
-			end
-		else
-			awful.util.spawn_with_shell("amixer -q set " .. alsa.channel .. ",0 off")
-			awful.util.spawn_with_shell("amixer -q set " .. alsa.channel .. ",1 off")
-		end
-		if alsa.volume.status == 'off' then
-			alsa.volume.status = 'on'
-		else
-			alsa.volume.status = 'off'
-		end
-		alsa.update_indicator()
-	end
+  function alsa.post_volume()
+    helpers.set_map("volume in progress", false)
+    alsa.update_indicator()
+  end
 
-	function alsa.toggle_mic()
-		awful.util.spawn("amixer -q set " .. alsa.mic_channel .. ",0 toggle")
-		awful.util.spawn("amixer -q set " .. alsa.mic_channel .. ",1 toggle")
-		--alsa.update_indicator()
-	end
+  function alsa.toggle()
+    if alsa.volume.status == 'off' then
+      for _, channel in pairs(alsa.channels_toggle) do 
+        awful.util.spawn_with_shell("amixer -q set " .. channel .. ",0 on")
+      end
+    else
+      awful.util.spawn_with_shell("amixer -q set " .. alsa.channel .. ",0 off")
+    end
+    if alsa.volume.status == 'off' then
+      alsa.volume.status = 'on'
+    else
+      alsa.volume.status = 'off'
+    end
+    alsa.update_indicator()
+  end
 
-	function alsa.update_indicator()
-		if alsa.volume.status == "off" then
-			alsa.widget:set_image(beautiful.widget_vol_mute)
-		elseif alsa.volume.level == 0 then
-			alsa.widget:set_image(beautiful.widget_vol_no)
-		elseif alsa.volume.level <= 50 then
-			alsa.widget:set_image(beautiful.widget_vol_low)
-		elseif alsa.volume.level <= 75 then
-			alsa.widget:set_image(beautiful.widget_vol)
-		else
-			alsa.widget:set_image(beautiful.widget_vol_high)
-		end
-		alsa.widget:set_text(
-			string.format(
-				"%-4s",
-				alsa.volume.level .. "%"
-		))
-	end
+  function alsa.toggle_mic()
+    awful.util.spawn("amixer -q set " .. alsa.mic_channel .. ",0 toggle")
+  end
 
-	function alsa.update()
-		asyncshell.request('amixer get ' .. alsa.channel,
-		                   function(f) alsa.post_update(f) end)
-	end
+  function alsa.update_indicator()
+    if alsa.volume.status == "off" then
+      alsa.widget:set_image(beautiful.widget_vol_mute)
+    elseif alsa.volume.level == 0 then
+      alsa.widget:set_image(beautiful.widget_vol_no)
+    elseif alsa.volume.level <= 50 then
+      alsa.widget:set_image(beautiful.widget_vol_low)
+    elseif alsa.volume.level <= 75 then
+      alsa.widget:set_image(beautiful.widget_vol)
+    else
+      alsa.widget:set_image(beautiful.widget_vol_high)
+    end
+    alsa.widget:set_text(
+      string.format(
+        "%-4s",
+        alsa.volume.level .. "%"
+    ))
+  end
 
-	function alsa.post_update(lines)
-		level, alsa.volume.status = string.match(
-			table.concat(lines, '\n'),
-                        "([%d]+)%%.*%[([%l]*)")
-		alsa.volume.level = tonumber(level) or nil
+  function alsa.update()
+    asyncshell.request(
+      'amixer get ' .. alsa.channel,
+      function(f) alsa.post_update(f) end)
+  end
 
-		if alsa.volume.level == nil
-		then
-			alsa.volume.level  = 0
-			alsa.volume.status = "off"
-		end
+  function alsa.post_update(lines)
+    level, alsa.volume.status = string.match(
+      table.concat(lines, '\n'),
+      "([%d]+)%%.*%[([%l]*)")
+    alsa.volume.level = tonumber(level) or nil
 
-		if alsa.volume.status == ""
-		then
-			if alsa.volume.level == 0
-			then
-				alsa.volume.status = "off"
-			else
-				alsa.volume.status = "on"
-			end
-		end
+    if alsa.volume.level == nil
+    then
+      alsa.volume.level  = 0
+      alsa.volume.status = "off"
+    end
 
-		alsa.update_indicator()
-	end
+    if alsa.volume.status == ""
+    then
+      if alsa.volume.level == 0
+      then
+        alsa.volume.status = "off"
+      else
+        alsa.volume.status = "on"
+      end
+    end
 
-	helpers.newtimer("alsa", alsa.timeout, alsa.update)
-	return setmetatable(alsa, { __index = alsa.widget })
+    alsa.update_indicator()
+  end
+
+  helpers.newtimer("alsa", alsa.timeout, alsa.update)
+  return setmetatable(alsa, { __index = alsa.widget })
 end
 
 return setmetatable(alsa, { __call = function(_, ...) return worker(...) end })
