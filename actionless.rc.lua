68d60f82b42c705cf16411ad8020f03e27ee55dc
actionless <actionless.loveless@gmail.com>
2014-05-19 18:45:01 +0200

    reogranized signals and bars a little

--- actionless.rc.lua
+++ actionless.rc.lua
@@ -211,26 +211,31 @@ end)
 --client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 --client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
-client.connect_signal("focus",
-	function(c)
-		local clients = awful.client.visible(s)
+client.connect_signal("focus", function(c)
+  if c.maximized_horizontal == true and c.maximized_vertical == true then
+    -- maximized
+    bars.remove_border(c)
+  elseif awful.client.floating.get(c) then
+    -- floating client
+    bars.make_titlebar(c)
+  elseif awful.layout.get(c.screen) == awful.layout.suit.floating then
+    -- floating layout
+    bars.make_titlebar(c)
+  else
+    bars.remove_titlebar(c)
+    bars.make_border(c)
+  end
+end)
 
-		if c.maximized_horizontal == true and c.maximized_vertical == true then
-			bars.remove_border(c)
-		elseif awful.client.floating.get(c)
-			or awful.layout.get(c.screen) == awful.layout.suit.floating then
-		--elseif awful.layout.get(c.screen) == awful.layout.suit.floating then
-			bars.make_titlebar(c)
-		else
-			bars.remove_titlebar(c)
-			bars.make_border(c, #clients)
-		end
-	end)
 client.connect_signal("unfocus", function(c)
-		if awful.client.floating.get(c) or awful.layout.get(c.screen) == awful.layout.suit.floating then
-			c.border_color = beautiful.titlebar
-		else
-			c.border_color = beautiful.border_normal
-		end
-	end)
+  if awful.client.floating.get(c) then
+    -- floating client
+    c.border_color = beautiful.titlebar
+  elseif awful.layout.get(c.screen) == awful.layout.suit.floating then
+    -- floating layout
+    c.border_color = beautiful.titlebar
+  else
+    c.border_color = beautiful.border_normal
+  end
+end)
 -- }}}
--- widgets/bars.lua
+++ widgets/bars.lua
@@ -8,7 +8,8 @@ local beautiful = require("widgets.helpers").beautiful
 bars = {}
 
 
-function bars.make_border(c, num_clients)
+function bars.make_border(c)
+        local num_clients = awful.client.visible(s)
 	if not settings.gtk3_app_classes[c.class] then
 		c.border_width = beautiful.border_width
 	end
@@ -53,23 +54,23 @@ function bars.make_titlebar(c)
 			awful.mouse.client.resize(c)
 		end)
 		)
-	-- Widgets that are aligned to the left
+
 	local left_layout = wibox.layout.fixed.horizontal()
 	left_layout:add(awful.titlebar.widget.closebutton(c))
 	left_layout:add(awful.titlebar.widget.minimizebutton(c))
 	--left_layout:add(awful.titlebar.widget.maximizedbutton(c))
-	-- Widgets that are aligned to the right
+
 	local right_layout = wibox.layout.fixed.horizontal()
 	right_layout:add(awful.titlebar.widget.ontopbutton(c))
 	right_layout:add(awful.titlebar.widget.stickybutton(c))
-	-- The title goes in the middle
+
 	local middle_layout = wibox.layout.flex.horizontal()
 	local title = awful.titlebar.widget.titlewidget(c)
 	title:set_align("center")
 	title:set_font(beautiful.titlebar_font)
 	middle_layout:add(title)
 	middle_layout:buttons(buttons)
-	-- Now bring it all together
+
 	local layout = wibox.layout.align.horizontal()
 	layout:set_left(left_layout)
 	layout:set_right(right_layout)
@@ -80,7 +81,7 @@ end
 
 function bars.titlebar_toggle(c)
 	if (c:titlebar_top():geometry()['height'] > 0) then
-		awful.titlebar(c, {size = 0})
+		bars.remove_titlebar(c)
 	else
 		bars.make_titlebar(c)
 	end
