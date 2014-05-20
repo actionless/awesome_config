6491b478e04b088b77aaad7134c6fb2ca3497663
actionless <actionless.loveless@gmail.com>
2014-05-20 16:17:48 +0200

    reorganized signals and rules to separate modules

---
+++ config/signals.lua
@@ -0,0 +1,55 @@
+local awful = require("awful")
+local client = require("client")
+
+local bars	= require("actionless.bars")
+local helpers	= require("actionless.helpers")
+local beautiful	= helpers.beautiful
+
+
+local signals = {}
+
+function signals.init()
+-- {{{ Signals
+
+-- Signal function to execute when a new client appears.
+client.connect_signal("manage", function (c, startup)
+	if not startup and not c.size_hints.user_position
+	   and not c.size_hints.program_position then
+		awful.placement.no_overlap(c)
+		awful.placement.no_offscreen(c)
+	end
+end)
+--client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+--client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+client.connect_signal("focus", function(c)
+  if c.maximized_horizontal == true and c.maximized_vertical == true then
+    -- maximized
+    bars.remove_border(c)
+  elseif awful.client.floating.get(c) then
+    -- floating client
+    bars.make_titlebar(c)
+  elseif awful.layout.get(c.screen) == awful.layout.suit.floating then
+    -- floating layout
+    bars.make_titlebar(c)
+  else
+    bars.remove_titlebar(c)
+    bars.make_border(c)
+  end
+end)
+
+client.connect_signal("unfocus", function(c)
+  if awful.client.floating.get(c) then
+    -- floating client
+    c.border_color = beautiful.titlebar
+  elseif awful.layout.get(c.screen) == awful.layout.suit.floating then
+    -- floating layout
+    c.border_color = beautiful.titlebar
+  else
+    c.border_color = beautiful.border_normal
+  end
+end)
+
+end
+-- }}}
+return signals
