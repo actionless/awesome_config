63ab9e7035783f794ae53b9a5260423fa3e22b19
actionless <actionless.loveless@gmail.com>
2014-05-20 15:07:51 +0200

    code style: beautified menu addon

--- widgets/menu_addon.lua
+++ widgets/menu_addon.lua
@@ -5,8 +5,7 @@
 
 local menu = require("awful.menu")
 local tags = require("awful.tag")
-local capi = {
-    client = client }
+local capi = { client = client }
 local escape_f = require("awful.util").escape
 
 local table_merge = require("widgets.helpers").imerge
@@ -16,47 +15,45 @@ local menu_addon = { mt={} }
 
 
 function menu_addon.clients_on_tag(args, item_args)
-    local cls = capi.client.focus
-    local cls_t = {}
+  local cls = capi.client.focus
+  local cls_t = {}
 
-    local all_tags = tags.gettags(cls.screen)
-    for _, t in ipairs(all_tags) do
-        if t.selected then
-            clients = t.clients(t)
-            for _, c in ipairs(clients) do
-                cls_t[#cls_t + 1] = {
-                    escape_f(c.name) or "",
-                    function ()
-                        if not c:isvisible() then
-                            tags.viewmore(c:tags(), c.screen)
-                        end
-                        capi.client.focus = c
-                        c:raise()
-                    end,
-                    c.icon }
-                if item_args then
-                    if type(item_args) == "function" then
-                        table_merge(cls_t[#cls_t], item_args(c))
-                    else
-                        table_merge(cls_t[#cls_t], item_args)
-                    end
-                end
-            end
-        end
+  local selected_tags = tags.selectedlist(cls.screen)
+  for _, t in ipairs(selected_tags) do
+    local clients = t.clients(t)
+    for _, c in ipairs(clients) do
+      table.insert(cls_t, {
+        escape_f(c.name) or "",
+        function ()
+          -- @TODO: i think it can be safely deleted:
+          --if not c:isvisible() then
+          --  tags.viewmore(c:tags(), c.screen)
+          --end
+          capi.client.focus = c
+          c:raise()
+        end,
+        c.icon
+      })
     end
-    args = args or {}
-    args.items = args.items or {}
-    table_merge(args.items, cls_t)
+    if item_args then
+      if type(item_args) == "function" then
+        table_merge(cls_t[#cls_t], item_args(c))
+      else
+        table_merge(cls_t[#cls_t], item_args)
+      end
+    end
+  end
+  args = args or {}
+  args.items = args.items or {}
+  table_merge(args.items, cls_t)
 
-    local m = menu.new(args)
-    m:show(args)
-    return m
+  local m = menu.new(args)
+  m:show(args)
+  return m
 end
 
 function menu_addon.mt:__call(...)
-    return menu.new(...)
+  return menu.new(...)
 end
 
 return setmetatable(menu_addon, menu_addon.mt)
-
--- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=4:softtabstop=4:textwidth=80
