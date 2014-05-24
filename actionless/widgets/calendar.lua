e3ea1a25a4865f290827b0bcf88f0203da3025ae
actionless <actionless.loveless@gmail.com>
2014-05-24 11:29:32 +0200

    codestyle: widgets: calendar

--- actionless/widgets/calendar.lua
+++ actionless/widgets/calendar.lua
@@ -1,120 +1,113 @@
-
 --[[
-                                                  
-     Licensed under GNU General Public License v2 
-      * (c) 2013, Luke Bonham                     
-                                                  
+   Licensed under GNU General Public License v2 
+        * (c) 2013, Luke Bonham
 --]]
 
-local helpers      = require('actionless.helpers')
-local mono_preset  = helpers.mono_preset
+local awful = require("awful")
+local beautiful = require("beautiful")
+local naughty = require("naughty")
 
-local awful        = require("awful")
-local beautiful    = require("beautiful")
-local naughty      = require("naughty")
-
-local io           = io
-local os           = { date = os.date }
-local tonumber     = tonumber
-
+local io = io
+local os = { date = os.date }
+local tonumber = tonumber
 local setmetatable = setmetatable
+
+local helpers = require('actionless.helpers')
+local mono_preset = helpers.mono_preset
+
+
 -- Calendar notification
 local calendar = {}
-local cal_notification = nil
+calendar.notification = nil
 
 function calendar:hide()
-    if cal_notification ~= nil then
-        naughty.destroy(cal_notification)
-        cal_notification = nil
-    end
+  if self.notification ~= nil then
+    naughty.destroy(self.notification)
+    self.notification = nil
+  end
 end
 
 function calendar:show(t_out, inc_offset)
-    calendar:hide()
+  self:hide()
 
-    local offs = inc_offset or 0
-    local tims = t_out or 0
-    local f, c_text
-    local today = tonumber(os.date('%d'))
-    local init_t = '/usr/bin/cal  | sed -r -e "s/(^| )( '
+  local offs = inc_offset or 0
+  local tims = t_out or 0
+  local f, c_text, c_title
+  local today = tonumber(os.date('%d'))
+  local init_t = '/usr/bin/cal  | sed -r -e "s/(^| )( '
 
-    if offs == 0
-    then -- current month showing, today highlighted
-        if today >= 10
-        then
-           init_t = '/usr/bin/cal  | sed -r -e "s/(^| )('
-        end
-
-        calendar.offset = 0
-        calendar.notify_icon = calendar.icons .. today .. ".png"
-
-        -- bg and fg inverted to highlight today
-        f = io.popen( init_t .. today ..
-                      ')($| )/\\1<b><span foreground=\\"'
-                      .. calendar.bg ..
-                      '\\" background=\\"'
-                      .. calendar.fg ..
-                      '\\">\\2<\\/span><\\/b>\\3/"' )
-
-    else -- no current month showing, no day to highlight
-       local month = tonumber(os.date('%m'))
-       local year = tonumber(os.date('%Y'))
-
-       calendar.offset = calendar.offset + offs
-       month = month + calendar.offset
-
-       if month > 12 then
-           month = month % 12
-           year = year + 1
-           if month <= 0 then
-               month = 12
-           end
-       elseif month < 1 then
-           month = month + 12
-           year = year - 1
-           if month <= 0 then
-               month = 1
-           end
-       end
-
-       calendar.notify_icon = nil
-
-       f = io.popen('/usr/bin/cal ' .. month .. ' ' .. year)
+  if offs == 0
+  then -- current month showing, today highlighted
+    if today >= 10 then
+      init_t = '/usr/bin/cal  | sed -r -e "s/(^| )('
     end
+    self.offset = 0
+    self.notify_icon = self.icons .. today .. ".png"
+    -- bg and fg inverted to highlight today
+    f = io.popen(
+      init_t .. today .. ')($| )' ..
+      '/\\1' ..
+      '<b>' ..
+      '<span foreground=\\"' .. self.bg .. '\\"' ..
+         ' background=\\"' .. self.fg .. '\\">' ..
+        '\\2' ..
+      '<\\/span>' ..
+      '<\\/b>' ..
+      '\\3/"' )
 
-    c_text = "<tt><b>"
-             .. f:read() .. "</b>\n\n"
-             .. f:read() .. "\n"
-             .. f:read("*all"):gsub("\n*$", "")
-             .. "</tt>"
- 
-    f:close()
+  else -- no current month showing, no day to highlight
+    local month = tonumber(os.date('%m'))
+    local year = tonumber(os.date('%Y'))
+    self.offset = self.offset + offs
+    month = month + self.offset
+    if month > 12 then
+      month = month % 12
+      year = year + 1
+      if month <= 0 then month = 12 end
+    elseif month < 1 then
+      month = month + 12
+      year = year - 1
+      if month <= 0 then month = 1 end
+    end
+    self.notify_icon = nil
+    f = io.popen('/usr/bin/cal ' .. month .. ' ' .. year)
+  end
 
-    cal_notification = naughty.notify({ text = c_text,
-                                        icon = calendar.notify_icon,
-                                        position = calendar.position,
-                                        fg = calendar.fg,
-                                        bg = calendar.bg,
-                                        timeout = tims,
-    preset = mono_preset })
+  c_title = f:read()
+  c_text = 
+    "<tt>" ..
+    f:read() .. "\n" ..
+    f:read("*all"):gsub("\n*$", "") ..
+    "</tt>"
+  f:close()
+  self.notification = naughty.notify({
+    title = c_title,
+    text = c_text,
+    icon = self.notify_icon,
+    position = self.position,
+    fg = self.fg,
+    bg = self.bg,
+    timeout = tims,
+    preset = mono_preset
+  })
 end
 
 function calendar:attach(widget, args)
-    local args = args or {}
-    calendar.icons = args.icons or beautiful.icons_dir .. "calendar/white/"
-    calendar.fg = args.fg or beautiful.fg_normal or "#FFFFFF"
-    calendar.bg = args.bg or beautiful.bg_normal or "#FFFFFF"
-    calendar.position = args.position or "top_right"
+  local args = args or {}
+  self.icons = args.icons or beautiful.icons_dir .. "calendar/white/"
+  self.fg = args.fg or beautiful.fg_normal or "#FFFFFF"
+  self.bg = args.bg or beautiful.bg_normal or "#FFFFFF"
+  self.position = args.position or "top_right"
 
-    calendar.offset = 0
-    calendar.notify_icon = nil
+  self.offset = 0
+  self.notify_icon = nil
 
-    widget:connect_signal("mouse::enter", function () calendar:show() end)
-    widget:connect_signal("mouse::leave", function () calendar:hide() end)
-    widget:buttons(awful.util.table.join( awful.button({ }, 1, function ()
-                                              calendar:show(0, -1) end),
-                                          awful.button({ }, 3, function ()
-                                              calendar:show(0, 1) end) ))
+  widget:connect_signal("mouse::enter", function () self:show() end)
+  widget:connect_signal("mouse::leave", function () self:hide() end)
+  widget:buttons(awful.util.table.join(
+    awful.button({ }, 1, function () self:show(0, -1) end),
+    awful.button({ }, 3, function () self:show(0,  1) end)
+  ))
 end
 
 return setmetatable(calendar, { __call = function(_, ...) return create(...) end })
