e59028ea464d19dbed966f0a7187dd6eb3f5c87e
actionless <actionless.loveless@gmail.com>
2014-05-29 17:15:26 +0200

    theme: updated lcars theme

--- themes/lcars/theme.lua
+++ themes/lcars/theme.lua
@@ -1,150 +1,184 @@
-theme                               = {}
+theme = {}
 
-themes_dir                          = os.getenv("HOME") .. "/.config/awesome/themes/lcars"
-theme.dir			= themes_dir
-theme.wallpaper_cmd                     = "hsetroot"
+themes_dir = os.getenv("HOME") .. "/.config/awesome/themes/lcars"
+theme.dir = themes_dir
+theme.wallpaper_cmd     = "hsetroot"
 
-theme.bg	= "#000000"
-theme.alt_bg	= "#000000"
+theme.bg		= "#000000"
+theme.alt_bg		= "#9966ff"
 
-theme.fg	= "#ffcc99"
-theme.alt_fg	= "#ffcc66"
-theme.shiny = "#000000"
+theme.fg		= "#ffcc99"
+theme.alt_fg		= "#ffcc66"
+theme.shiny		= "#000000"
 
-theme.light	= "#cc99cc"
-theme.dark	= "#cc6699"
-theme.error	= "#ff3300"
+theme.theme		= "#cc99cc"
+theme.error		= "#ff3300"
 
-theme.border	= "#000000"
-theme.sel_border = "#cc99cc"
-theme.titlebar	= "#000000"
+theme.border		= "#000000"
+theme.sel_border	= "#cc99cc"
+theme.titlebar		= "#000000"
 theme.titlebar_focus	= "#cc6699"
 
-theme.font                          = "Meslo LG S 10"
-theme.sans_font             		= "Ubuntu 8.9"
-theme.fg_normal                     = theme.fg
-theme.bg_normal                     = theme.bg
-theme.fg_focus                      = theme.error
-theme.bg_focus                      = theme.dark
-theme.fg_urgent                     = theme.bg
-theme.bg_urgent                     = theme.error
+--theme.font		= "DejaVu Sans Mono 9"
+--theme.font		= "Dina 8"
+--theme.font		= "Fira Mono 9"
+--theme.font		= "Meslo LG S 10"
+theme.font		= "monoOne 10"
+--theme.font		= "PT Mono 8"
+--theme.font		= "Share Tech Mono 11"
+--theme.font		= "Source Code Pro Medium 9.4"
+--theme.font		= "tewi 9"
 
-theme.border_width                  = "3"
-theme.border_normal                 = theme.border
-theme.border_focus                  = theme.sel_border
-theme.border_marked                 = theme.error
+theme.sans_font		= "PT Sans 10"
+--theme.sans_font	= "Share Tech 11"
+--theme.sans_font	= "Source Sans Pro Regular 10"
 
-theme.taglist_font                  = theme.font
-theme.taglist_fg_focus              = theme.shiny
-theme.taglist_bg_focus              = theme.dark
+theme.fg_normal			= theme.fg
+theme.bg_normal			= theme.bg
+theme.fg_focus			= theme.fg
+theme.bg_focus			= theme.theme
+theme.fg_urgent			= theme.bg
+theme.bg_urgent			= theme.error
 
-theme.tasklist_font                 = theme.font
-theme.tasklist_fg_focus             = theme.light
-theme.tasklist_bg_focus             = theme.bg
-theme.tasklist_fg_normal            = theme.fg
-theme.tasklist_bg_normal            = theme.bg
-theme.tasklist_fg_minimize          = theme.shiny
-theme.tasklist_bg_minimize          = "#6666ff"
+theme.screen_margin		= 0
 
-theme.titlebar_font                 = "Ubuntu Bold 9"
-theme.titlebar_fg_focus             = theme.shiny
-theme.titlebar_fg_normal            = theme.fg
-theme.titlebar_bg_focus             = theme.titlebar_focus
-theme.titlebar_bg_normal            = theme.titlebar
+theme.border_width		= "12"
+theme.border_normal		= theme.border
+theme.border_focus		= theme.sel_border
+theme.border_marked		= theme.error
 
-theme.notification_opacity = 0.8
-theme.notification_font             = theme.sans_font
-theme.notification_monofont         = theme.font
-theme.notify_fg                     = theme.fg_normal
-theme.notify_bg                     = theme.bg_normal
-theme.notify_border                 = theme.border_focus
+theme.panel_opacity		= 0.9
 
-theme.textbox_widget_margin_top     = 1
-theme.awful_widget_height           = 14
-theme.awful_widget_margin_top       = 2
-theme.mouse_finder_color            = theme.error
-theme.menu_height                   = "16"
-theme.menu_width                    = "140"
+theme.taglist_font		= theme.font
+theme.taglist_fg_focus		= theme.shiny
+theme.taglist_bg_focus		= theme.theme
 
-theme.mpd_text                      = theme.light
+theme.tasklist_font		= theme.sans_font
+theme.tasklist_fg_focus		= theme.fg
+theme.tasklist_bg_focus		= theme.bg
+theme.tasklist_fg_normal	= theme.fg
+theme.tasklist_bg_normal	= theme.bg
+theme.tasklist_fg_minimize	= theme.bg
+theme.tasklist_bg_minimize	= "#6c6c6c"
+
+--theme.titlebar_font		= theme.sans_font
+theme.titlebar_font		= "PT Caption Bold 10.5"
+--theme.titlebar_font		= "Source Sans Pro Bold 10.5"
+theme.titlebar_fg_focus		= theme.shiny
+theme.titlebar_fg_normal	= theme.bg
+theme.titlebar_bg_focus		= theme.titlebar_focus
+theme.titlebar_bg_normal	= theme.titlebar
+
+theme.notification_opacity	= 0.8
+theme.notification_font		= theme.sans_font
+theme.notification_monofont	= theme.font
+theme.notify_fg			= theme.fg_normal
+theme.notify_bg			= theme.bg_normal
+theme.notify_border		= theme.border_focus
+
+theme.textbox_widget_margin_top	= 1
+theme.awful_widget_height	= 14
+theme.awful_widget_margin_top	= 2
+theme.mouse_finder_color	= theme.error
+theme.menu_border_width		= "3"
+theme.menu_height		= "16"
+theme.menu_width		= "140"
+
+theme.player_text		= "#8d5f88"
 
 -- ICONS
 
-theme.taglist_squares_sel           = themes_dir .. "/icons/square_sel.png"
-theme.taglist_squares_unsel         = themes_dir .. "/icons/square_unsel.png"
+icons_dir = theme.dir .. "/icons/"
+theme.icons_dir = icons_dir
 
-theme.menu_submenu_icon             = themes_dir .. "/icons/submenu.png"
-theme.taglist_squares_sel           = themes_dir .. "/icons/square_sel.png"
-theme.taglist_squares_unsel         = themes_dir .. "/icons/square_unsel.png"
+theme.taglist_squares_sel	= icons_dir .. "square_sel.png"
+theme.taglist_squares_unsel	= icons_dir .. "square_unsel.png"
 
-theme.layout_tile                   = themes_dir .. "/icons/tile.png"
-theme.layout_tilegaps               = themes_dir .. "/icons/tilegaps.png"
-theme.layout_tileleft               = themes_dir .. "/icons/tileleft.png"
-theme.layout_tilebottom             = themes_dir .. "/icons/tilebottom.png"
-theme.layout_tiletop                = themes_dir .. "/icons/tiletop.png"
-theme.layout_fairv                  = themes_dir .. "/icons/fairv.png"
-theme.layout_fairh                  = themes_dir .. "/icons/fairh.png"
-theme.layout_spiral                 = themes_dir .. "/icons/spiral.png"
-theme.layout_dwindle                = themes_dir .. "/icons/dwindle.png"
-theme.layout_max                    = themes_dir .. "/icons/max.png"
-theme.layout_fullscreen             = themes_dir .. "/icons/fullscreen.png"
-theme.layout_magnifier              = themes_dir .. "/icons/magnifier.png"
-theme.layout_floating               = themes_dir .. "/icons/floating.png"
+theme.menu_submenu_icon		= icons_dir .. "submenu.png"
+theme.taglist_squares_sel	= icons_dir .. "square_sel.png"
+theme.taglist_squares_unsel	= icons_dir .. "square_unsel.png"
 
-theme.arrl                          = themes_dir .. "/icons/arrl.png"
-theme.arrl_dl                       = themes_dir .. "/icons/arrl_dl.png"
-theme.arrl_ld                       = themes_dir .. "/icons/arrl_ld.png"
+theme.arrl			= icons_dir .. "arrl.png"
 
-theme.widget_ac                     = themes_dir .. "/icons/ac.png"
-theme.widget_battery                = themes_dir .. "/icons/battery.png"
-theme.widget_battery_low            = themes_dir .. "/icons/battery_low.png"
-theme.widget_battery_empty          = themes_dir .. "/icons/battery_empty.png"
-theme.widget_mem                    = themes_dir .. "/icons/mem.png"
-theme.widget_cpu                    = themes_dir .. "/icons/cpu.png"
-theme.widget_temp                   = themes_dir .. "/icons/temp.png"
-theme.widget_net                    = themes_dir .. "/icons/net.png"
-theme.widget_hdd                    = themes_dir .. "/icons/hdd.png"
-theme.widget_music                  = themes_dir .. "/icons/note.png"
-theme.widget_music_on               = themes_dir .. "/icons/note_on.png"
-theme.widget_vol_high                    = themes_dir .. "/icons/vol_high.png"
-theme.widget_vol                    = themes_dir .. "/icons/vol.png"
-theme.widget_vol_low                = themes_dir .. "/icons/vol_low.png"
-theme.widget_vol_no                 = themes_dir .. "/icons/vol_no.png"
-theme.widget_vol_mute               = themes_dir .. "/icons/vol_mute.png"
-theme.widget_mail                   = themes_dir .. "/icons/mail.png"
-theme.widget_mail_on                = themes_dir .. "/icons/mail_on.png"
+theme.widget_ac			= icons_dir .. "ac.png"
+theme.widget_ac_charging	= icons_dir .. "ac_charging.png"
+theme.widget_ac_charging_low	= icons_dir .. "ac_charging_low.png"
 
-theme.dropdown_icon                = themes_dir .. "/icons/dropdown.png"
+theme.widget_battery		= icons_dir .. "battery.png"
+theme.widget_battery_low	= icons_dir .. "battery_low.png"
+theme.widget_battery_empty	= icons_dir .. "battery_empty.png"
 
-theme.tasklist_disable_icon         = true
---theme.tasklist_floating             = "*"
+theme.widget_mem		= icons_dir .. "mem.png"
+theme.widget_cpu		= icons_dir .. "cpu.png"
+theme.widget_temp		= icons_dir .. "temp.png"
+theme.widget_net		= icons_dir .. "net.png"
+theme.widget_hdd		= icons_dir .. "hdd.png"
+
+theme.widget_net_wireless	= icons_dir .. "net_wireless.png"
+theme.widget_net_wired		= icons_dir .. "net_wired.png"
+theme.widget_net_searching	= icons_dir .. "net_searching.png"
+
+theme.widget_music		= icons_dir .. "note.png"
+theme.widget_music_on		= icons_dir .. "note_on.png"
+theme.widget_vol_high		= icons_dir .. "vol_high.png"
+theme.widget_vol		= icons_dir .. "vol.png"
+theme.widget_vol_low		= icons_dir .. "vol_low.png"
+theme.widget_vol_no		= icons_dir .. "vol_no.png"
+theme.widget_vol_mute		= icons_dir .. "vol_mute.png"
+theme.widget_mail		= icons_dir .. "mail.png"
+theme.widget_mail_on		= icons_dir .. "mail_on.png"
+
+theme.dropdown_icon		= icons_dir .. "dropdown.png"
+
+theme.tasklist_disable_icon = true
+--theme.tasklist_floating = "*"
 --theme.tasklist_maximized_horizontal = "_"
---theme.tasklist_maximized_vertical   = "|"
+--theme.tasklist_maximized_vertical = "|"
 
-theme.titlebar_close_button_focus               = themes_dir .. "/icons/titlebar/close_focus.png"
-theme.titlebar_close_button_normal              = themes_dir .. "/icons/titlebar/close_normal.png"
+layout_icons_dir = icons_dir .. "layout/"
+theme.layout_icons_dir = layout_icons_dir
+theme.layout_tile		= layout_icons_dir .. "tile.png"
+theme.layout_tilegaps		= layout_icons_dir .. "tilegaps.png"
+theme.layout_tileleft		= layout_icons_dir .. "tileleft.png"
+theme.layout_tilebottom		= layout_icons_dir .. "tilebottom.png"
+theme.layout_tiletop		= layout_icons_dir .. "tiletop.png"
+theme.layout_fairv		= layout_icons_dir .. "fairv.png"
+theme.layout_fairh		= layout_icons_dir .. "fairh.png"
+theme.layout_spiral		= layout_icons_dir .. "spiral.png"
+theme.layout_dwindle		= layout_icons_dir .. "dwindle.png"
+theme.layout_max		= layout_icons_dir .. "max.png"
+theme.layout_fullscreen		= layout_icons_dir .. "fullscreen.png"
+theme.layout_magnifier		= layout_icons_dir .. "magnifier.png"
+theme.layout_floating		= layout_icons_dir .. "floating.png"
 
-theme.titlebar_ontop_button_focus_active        = themes_dir .. "/icons/titlebar/ontop_focus_active.png"
-theme.titlebar_ontop_button_normal_active       = themes_dir .. "/icons/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_inactive      = themes_dir .. "/icons/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_inactive     = themes_dir .. "/icons/titlebar/ontop_normal_inactive.png"
+titlebar_icons_dir = icons_dir .. "titlebar/"
+theme.titlebar_icons_dir = titlebar_icons_dir
+theme.titlebar_close_button_focus = titlebar_icons_dir .. "/close_focus.png"
+theme.titlebar_close_button_normal = titlebar_icons_dir .. "/close_normal.png"
 
-theme.titlebar_sticky_button_focus_active       = themes_dir .. "/icons/titlebar/sticky_focus_active.png"
-theme.titlebar_sticky_button_normal_active      = themes_dir .. "/icons/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_inactive     = themes_dir .. "/icons/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_inactive    = themes_dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_ontop_button_focus_active = titlebar_icons_dir .. "/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = titlebar_icons_dir .. "/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive = titlebar_icons_dir .. "/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = titlebar_icons_dir .. "/ontop_normal_inactive.png"
 
-theme.titlebar_floating_button_focus_active     = themes_dir .. "/icons/titlebar/floating_focus_active.png"
-theme.titlebar_floating_button_normal_active    = themes_dir .. "/icons/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_inactive   = themes_dir .. "/icons/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_inactive  = themes_dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active = titlebar_icons_dir .. "/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = titlebar_icons_dir .. "/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive = titlebar_icons_dir .. "/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = titlebar_icons_dir .. "/sticky_normal_inactive.png"
 
-theme.titlebar_maximized_button_focus_active    = themes_dir .. "/icons/titlebar/maximized_focus_active.png"
-theme.titlebar_maximized_button_normal_active   = themes_dir .. "/icons/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_dir .. "/icons/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_inactive = themes_dir .. "/icons/titlebar/maximized_normal_inactive.png"
+theme.titlebar_floating_button_focus_active = titlebar_icons_dir .. "/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = titlebar_icons_dir .. "/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive = titlebar_icons_dir .. "/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = titlebar_icons_dir .. "/floating_normal_inactive.png"
 
-theme.titlebar_minimized_button_focus               = themes_dir .. "/icons/titlebar/minimized_focus.png"
-theme.titlebar_minimized_button_normal              = themes_dir .. "/icons/titlebar/minimized_normal.png"
+theme.titlebar_maximized_button_focus_active = titlebar_icons_dir .. "/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = titlebar_icons_dir .. "/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive = titlebar_icons_dir .. "/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = titlebar_icons_dir .. "/maximized_normal_inactive.png"
+
+theme.titlebar_minimize_button_focus_active = titlebar_icons_dir .. "/minimized_focus.png"
+theme.titlebar_minimize_button_normal_active = titlebar_icons_dir .. "/minimized_normal.png"
+theme.titlebar_minimize_button_focus_inactive = titlebar_icons_dir .. "/minimized_focus.png"
+theme.titlebar_minimize_button_normal_inactive = titlebar_icons_dir .. "/minimized_normal.png"
 
 return theme
