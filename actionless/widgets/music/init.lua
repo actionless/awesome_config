bc4a40f15a2fc72d2eb505fe99e478385fc4ac91
actionless <actionless.loveless@gmail.com>
2014-06-29 11:16:09 +0200

    widgets: music: moved tag parser to a separate file

--- actionless/widgets/music/init.lua
+++ actionless/widgets/music/init.lua
@@ -1,6 +1,7 @@
 local music = {
   backends	= require("actionless.widgets.music.backends"),
-  widget	= require("actionless.widgets.music.widget")
+  widget	= require("actionless.widgets.music.widget"),
+  tag_parser	= require("actionless.widgets.music.tag_parser")
 }
 
 return music
---
+++ actionless/widgets/music/tag_parser.lua
@@ -0,0 +1,115 @@
+local tag_parser = {}
+
+  function tag_parser.predict_missing_tags(player_status)
+
+    local naughty = {notify = function()end}
+
+    if player_status.file then
+      player_status.file = player_status.file:match("^.*://(.*)$")
+        or player_status.file
+    else
+      player_status.file = N_A
+    end
+    if player_status.cover then
+      player_status.cover = player_status.cover:match("^file://(.*)$")
+        or player_status.file
+    else
+      player_status.cover = default_art or N_A
+    end
+
+    if player_status.file == N_A or player_status.file == ''
+    then
+      return player_status
+    end
+
+    local a
+    --1
+    a = player_status.file:match("^.*[/](.*)[/]%d+[-%. ].*[/]")
+    if a then naughty.notify({text="*/(Artist Name)/Year - Album/*"}) else
+    --2
+    a = player_status.file:match("^.*[/]%d+ [-] (.*) [-] .*[.].+")
+    if a then naughty.notify({text=2}) else
+    --3
+    a = player_status.file:match("^(.*)[/]%d+ [-] .*[/]")
+    if a then naughty.notify({text=3}) else
+    --4
+    a = player_status.file:match(".*[/].*[/](.*)[/].*[.].+$")
+    if a then naughty.notify({text="/path/to/(Artist or VA)/Song name.ext"}) else
+    --5
+    a = player_status.file:match("^.*[/]([!/]*)[/][!/]*[.].+$")
+    if a then naughty.notify({text=5}) else
+    --6
+    a = player_status.file:match("^(.*)[/]%d+ [-] .*[/]")
+    if a then naughty.notify({text=6}) else
+    --7
+    a = player_status.file:match("^.*[/](.*) [-] [!/]*[.].+")
+    if a then naughty.notify({text=7}) else
+    --8
+    a = player_status.file:match("^(.*)[/]%d+ [-] .*")
+    if a then naughty.notify({text=8}) else
+    --9
+    a = player_status.file:match("^(.*)[/].*")
+    if a then a = ''; naughty.notify({text=9})
+    end --9
+    end --8
+    end --7
+    end --6
+    end --5
+    end --4
+    end --3
+    end --2
+    end --1
+
+    local t = player_status.title
+    local new_t
+    local f = player_status.file
+    naughty.notify({text=t})
+    if t:match('%.mp3') then
+      --1
+      new_t = t:match('^%d+[%. -%_]+(.*)%.mp3')
+      if new_t then naughty.notify({text="10. - (Song Title).mp3"}) else
+      --2
+      new_t = t:match('(.*)%.mp3')
+      if new_t then naughty.notify({text="(Song Title).mp3"}) else
+      new_t = f; naughty.notify({text="Song Title.mp3"})
+      end --1
+      end --2
+    else
+      --1
+      new_t = f:match(".*[/].* [-] (.*)[.].+$")
+      if t then naughty.notify({text="t1"}) else
+      --2
+      new_t = f:match(".*[/]%d+[ -]+(.*)[.].+$")
+      if t then naughty.notify({text="t2"}) else
+      --3
+      new_t = f:match(".*[/](.*) [.].*")
+      if t then naughty.notify({text="t3"}) else
+      --4
+      new_t = f:match(".*[/](.*)[.].*")
+      if t then naughty.notify({text="t4"}) else
+      new_t = f; naughty.notify({text="t5"})
+      end --4
+      end --3
+      end --2
+      end --1
+    end
+
+    if not t or #t == 0 or t:match('%.mp3') then
+      player_status.title = new_t
+    end
+    if not player_status.artist or #player_status.artist == 0 then
+      player_status.artist = a
+    end
+
+    -- let's insert placeholders for all the missing fields
+    for _, k in ipairs({
+      'file', 'locationartist', 'title', 'album', 'date', 'cover', 'artist',
+    }) do
+      if not player_status[k] then
+        player_status[k] = N_A
+      end
+    end
+    return player_status
+  end
+-------------------------------------------------------------------------------
+return tag_parser
