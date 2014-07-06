a5196efd01683c5310299b35fdaa2ae16bd18531
actionless <actionless.loveless@gmail.com>
2014-07-06 15:51:37 +0200

    themes: lcars_modern: sans font

--- themes/lcars_modern/theme.lua
+++ themes/lcars_modern/theme.lua
@@ -58,7 +58,8 @@ theme.font		= "Share Tech Mono Bold 10.5"
 --theme.font		= "LCARS 17"
 --theme.font		= "Fira Mono 8"
 --theme.sans_font		= "Fira Sans Medium 10.5"
-theme.sans_font		= "Kelly Slab Bold 10.5"
+--theme.sans_font		= "Kelly Slab Bold 10.5"
+theme.sans_font		= "Share Tech Bold 11"
 --theme.sans_font		= theme.font
 
 theme.fg_normal			= theme.fg
