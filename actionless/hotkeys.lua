1acbe27466f21e994d85412dff6f679c84ebed0a
actionless <actionless.loveless@gmail.com>
2014-08-29 08:46:33 +0200

    widgets: hotkeys: improved appearance

--- actionless/hotkeys.lua
+++ actionless/hotkeys.lua
@@ -5,12 +5,18 @@ local beautiful = require("beautiful")
 
 local helpers = require("actionless.helpers")
 local h_table = require("actionless.table")
-local h_string = require("actionless.string")
 local markup = require("actionless.markup")
-local common_widgets = require("actionless.widgets.common")
-local centered_widget = common_widgets.centered
-local bordered_widget = common_widgets.bordered
+local bordered_widget = require("actionless.widgets.common").bordered
 
+local APPEARANCE = {
+  width = 1400,
+  height = 600,
+  key_padding = 5,
+  key_margin = 5,
+  comment_font_size = 8,
+  comment_width_chars = 10,
+  pressed_color_group = 2,
+}
 local hotkeys = {
   bindings = {
     Mod4 = {
@@ -23,29 +29,21 @@ local hotkeys = {
     visible = false,
   }
 }
-local APPEARANCE = {
-  width = 1400,
-  height = 600,
-  key_padding = 5,
-  key_margin = 5,
-  comment_font_size = 8,
-  comment_width_chars = 10
-}
 local KEYBOARD = {
   { 'Escape', '#67', '#68', '#69', '#70', '#71', '#72', '#73', '#74', '#75', '#76', '#95', '#96', 'Home', 'End'},
   { '`', '#10', '#11', '#12', '#13', '#14', '#15', '#16', '#17', '#18', '#19', '#20', '#21', 'Insert', 'Delete' },
-  { 'Tab',  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Backspace'  },
-  { 'Caps',  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\', 'Return' },
-  { 'Shift',  'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Next', 'Up' , 'Prior' },
-  { 'Fn', 'Control', 'Mod4', 'Mod1', '    ', 'space', '   ', '    ', 'Alt Gr', 'Print', 'Control', 'Left', 'Down', 'Right'},
+  { 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Backspace' },
+  { 'Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\', 'Return' },
+  { 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Next', 'Up' , 'Prior' },
+  { 'Fn', 'Control', 'Mod4', 'Mod1', '', 'space', '', '', 'Alt Gr', 'Print', 'Control', 'Left', 'Down', 'Right'},
 }
 local KEYBOARD_LABELS = {
   { 'Esc', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Home', 'End'},
   { '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Ins', 'Del'},
-  { 'Tab',  'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']',   'Backspace    ' },
-  { 'Caps',  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\', 'Enter        ' },
-  { 'Shift',  'z', 'x', 'c', 'v', 'b', 'n', 'm', '&lt;', '&gt;', '?', 'PgUp', 'Up' , 'PgDn' },
-  { 'Fn', 'Ctrl', 'Super', 'Alt', '    ', 'Space', '    ', '    ', 'Alt G', 'PrScr', 'Ctrl', 'Left', 'Down', 'Right'},
+  { 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'BackSpc' },
+  { 'Caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", '\\', 'Enter' },
+  { 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '&lt;', '&gt;', '?', 'PgUp', 'Up' , 'PgDn' },
+  { 'Fn', 'Ctrl', 'Super', 'Alt', '', 'Space', '', '', 'Alt G', 'PrScr', 'Ctrl', 'Left', 'Down', 'Right'},
 }
 local SPECIAL_KEYBUTTONS = {
   'Esc',
@@ -59,18 +57,35 @@ local SPECIAL_KEYBUTTONS = {
   'PrScr',
   'PgUp',
   'PgDn',
-  'Backspace    ',
-  'Enter        ',
+  'BackSpc',
+  'Enter',
   'Ins',
   'Del',
   'Home',
-  'End'
+  'End',
+  'F1',
+  'F2',
+  'F3',
+  'F4',
+  'F5',
+  'F6',
+  'F7',
+  'F8',
+  'F9',
+  'F10',
+  'F11',
+  'F12',
 }
+
 local MODIFIERS = {
   Control = '#37'
 }
-
 local function keyname_to_keycode(keyname)
+  --[[
+  by default modifier can't be binded as a hotkey, only as a modifier.
+  but it's possible to bind it if use it's keycode instead of alias,
+  like '#37' instead of 'Control'
+  --]]
   for k, v in pairs(MODIFIERS) do
     if k == keyname then return v end
   end
@@ -83,10 +98,6 @@ local function get_mod_table_name(modifiers)
   return table.concat(copied)
 end
 
-local function human_readable_modifiers(modifiers)
-  return table.concat(modifiers, '+')
-end
-
 
 local function new_keybutton(key_label, comment, key_group)
   local obj = {}
@@ -97,11 +108,7 @@ local function new_keybutton(key_label, comment, key_group)
   ))
   local comment_widget = wibox.widget.textbox()
   comment_widget:set_font(helpers.font .. " " .. APPEARANCE.comment_font_size)
-  comment_widget:set_text(
-      comment
-      and h_string.multiline_limit(comment, APPEARANCE.comment_width_chars)
-      or string.rep(" ", APPEARANCE.comment_width_chars)
-  )
+  comment_widget:set_text(comment or '')
 
   local button_layout = wibox.layout.fixed.vertical()
   button_layout:add(letter_widget)
@@ -135,24 +142,27 @@ end
 
 local function init_keyboard(modifiers)
   local modifiers_table_name = get_mod_table_name(modifiers)
-  local keyboard_layout = wibox.layout.fixed.vertical()
-  local modifiers_title_row = wibox.layout.fixed.horizontal()
-  modifiers_title_row:add(wibox.widget.textbox(
-    markup.big(
-      human_readable_modifiers(modifiers) .. '+__'
-  )))
-  keyboard_layout:add(modifiers_title_row)
-  for i1,row in ipairs(KEYBOARD) do
-    local row_layout = wibox.layout.fixed.horizontal()
-    for i2,key in ipairs(row) do
+
+  for i, modifier in ipairs(modifiers) do
+    hotkeys.bindings[modifiers_table_name][modifier] = {
+      comment='pressed',
+      group=APPEARANCE.pressed_color_group,
+    }
+  end
+
+  local keyboard_layout = wibox.layout.flex.vertical()
+  for i1, row in ipairs(KEYBOARD) do
+    local row_layout = wibox.layout.flex.horizontal()
+    for i2, key in ipairs(row) do
       local hotkey_record = hotkeys.bindings[modifiers_table_name][key] or
-                            { comment=nil, group=nil}
+                            { comment=nil, group=nil }
       row_layout:add(new_keybutton(
         KEYBOARD_LABELS[i1][i2], hotkey_record.comment, hotkey_record.group
       ))
     end
     keyboard_layout:add(row_layout)
   end
+
   return keyboard_layout
 end
 
@@ -161,24 +171,24 @@ local function init_popup(modifiers)
   local scrgeom = capi.screen[helpers.get_current_screen()].workarea
   local width = APPEARANCE.width
   local height = APPEARANCE.height
-  local x = (scrgeom.width - width)/2
-  local y = (scrgeom.height - height)/2
+  local x = (scrgeom.width - width) / 2
+  local y = (scrgeom.height - height) / 2
 
-  local mywibox = wibox({})
-  mywibox.ontop = true
-  mywibox.opacity = beautiful.notification_opacity
-
-  local content_widget = wibox.widget.background()
-  mywibox:set_widget(centered_widget(content_widget))
-  content_widget:set_widget(init_keyboard(modifiers))
-
-  mywibox.visible = false
+  local mywibox = wibox({
+    ontop = true,
+    opacity = beautiful.notification_opacity,
+  })
   mywibox:geometry({
     x = x,
     y = y,
     height = height,
     width = width,
   })
+  mywibox:set_widget(
+    bordered_widget(init_keyboard(modifiers), {
+      padding = APPEARANCE.key_padding,
+      margin = APPEARANCE.key_margin,
+  }))
 
   return mywibox
 end
@@ -193,12 +203,15 @@ end
 function hotkeys.key(modifiers, key, key_press_function, key_release_function,
                      comment, key_group)
   local patched_key_press_function
+
   if key_press_function == 'show_help' then
+    -- {{ that needed if popup is called by modifier itself:
     local modifiers_to_show = h_table.deepcopy(modifiers)
     if h_table.contains_key(MODIFIERS, key) then
       table.insert(modifiers_to_show, key)
       key = keyname_to_keycode(key)
     end
+    -- }}
     patched_key_press_function = function()
       hotkeys.show_by_modifiers(modifiers_to_show)
     end
@@ -217,7 +230,6 @@ function hotkeys.key(modifiers, key, key_press_function, key_release_function,
     comment=comment,
     group=key_group,
   }
-  modifiers = modifiers or {}
 
   return awful.key(modifiers, key, patched_key_press_function, key_release_function)
 end
@@ -232,9 +244,10 @@ function hotkeys.show_by_modifiers(modifiers)
     if not hotkeys.cached_keyboards[mod_table] then
       hotkeys.cached_keyboards[mod_table] = init_popup(modifiers)
     end
-    hotkeys.popup.visible=false
+    local old_popup = hotkeys.popup
     hotkeys.popup = hotkeys.cached_keyboards[mod_table]
-    hotkeys.popup.visible=true
+    hotkeys.popup.visible = true
+    old_popup.visible = false
   else
     hotkeys.popup.visible = not hotkeys.popup.visible
   end
