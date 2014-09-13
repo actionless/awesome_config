bca8582cbc6adc89181175e6657dac834204eae8
actionless <actionless.loveless@gmail.com>
2014-09-13 06:59:19 +0200

    async: clean-up

--- actionless/async.lua
+++ actionless/async.lua
@@ -28,9 +28,12 @@ function async.execute(command, callback)
   awful.util.spawn_with_shell(string.format(
     [[
   echo async.pipe_multiline_done\(\"%q\", \""$(%s | %s)"\"\) | awesome-client;
+    --")-- syntax highlighter fix
     ]],
-    id, command:gsub('"','\"'), [[awk 1 ORS='\\\\n' | sed 's/"/\\"/g']]
-  )) --:-'")]]))  that beardy smiley is helping my stupid syntax highlighter
+    id,
+    command:gsub('"','\"'),
+    [[awk 1 ORS='\\\\n' | sed 's/"/\\"/g']]
+  ))
   return id
 end
 
@@ -40,45 +43,5 @@ function async.pipe_multiline_done(id, str)
   async.request_table[id].callback(str)
   async.request_table[id] = nil
 end
-
--- Sends an asynchronous request for an output of the shell command.
--- @param command Command to be executed and taken output from
--- @param callback Function to be called when the command finishes
--- @return Request ID
-function async.execute_iter(command, callback)
-  local id = next_id()
-  async.request_table[id] = {
-    callback = callback,
-    table = {}}
-  awful.util.spawn_with_shell(string.format(
-    [[ sh -c '
-       %s | while read line; do
-         echo "async.pipe_consume(\"%q\", \"$line\")" | awesome-client;
-       done
-       echo "async.pipe_finish(\"%q\")" | awesome-client;'
-    ]],
-    string.gsub(command, "'", "'\\''"), id, id
-  ))
-  return id
-end
-
--- Consumes command's output line by line
--- @param id Request ID
--- @param line The next line of the command's output
-function async.pipe_consume(id, line)
-  if not async.request_table[id] then return end
-  async.request_table[id].table:insert(line)
-end
-
--- Calls the remembered callback function on the output of the shell
--- command.
--- @param id Request ID
-function async.pipe_finish(id)
-  if not async.request_table[id] then return end
-  async.request_table[id].callback(
-    async.request_table[id].table)
-  async.request_table[id] = nil
-end
-
 
 return async
