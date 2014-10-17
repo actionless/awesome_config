1befdfbdbdab58bbc6acf6a2acb7731941ee0232
actionless <actionless.loveless@gmail.com>
2014-10-17 17:19:11 +0200

    style(config: signals)

--- config/signals.lua
+++ config/signals.lua
@@ -8,15 +8,17 @@ local titlebar	= require("actionless.titlebar")
 local signals = {}
 
 function signals.init(status)
--- {{{ Signals
 
--- Signal function to execute when a new client appears.
+-- New client appears
 client.connect_signal("manage", function (c, startup)
-	if not startup and not c.size_hints.user_position
-	   and not c.size_hints.program_position then
-		awful.placement.no_overlap(c)
-		awful.placement.no_offscreen(c)
-	end
+  if
+    not startup and not c.size_hints.user_position
+  and
+    not c.size_hints.program_position
+  then
+      awful.placement.no_overlap(c)
+      awful.placement.no_offscreen(c)
+  end
 end)
 
 client.connect_signal("focus", function(c)
