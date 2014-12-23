46d8a5bfad3312b510bad2e902ef18980c319cac
actionless <actionless.loveless@gmail.com>
2014-12-23 14:39:46 +0100

    theme: lcars-xresources: use dark titlebar icons

--- themes/lcars_xresources/theme.lua
+++ themes/lcars_xresources/theme.lua
@@ -84,4 +84,9 @@ theme.tasklist_font = theme.sans_font
 theme.taglist_squares_sel       = nil
 theme.taglist_squares_unsel     = nil
 
+  theme.titlebar_fg_focus		= theme.titlebar_border
+  theme.titlebar_bg_focus		= theme.titlebar_focus_border
+  theme.titlebar_fg_normal	= theme.tasklist_fg_normal
+  theme.titlebar_bg_normal	= theme.titlebar_border
+
 return theme
