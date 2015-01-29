a17b5687e0588710e08861b95daa40017735cc9d
actionless <actionless.loveless@gmail.com>
2015-01-29 14:00:02 +0100

    chore(themes: lcars-xresources): small clean-up

--- themes/lcars_xresources/theme.lua
+++ themes/lcars_xresources/theme.lua
@@ -20,9 +20,6 @@ theme.panel_info = "theme.color.13"
 theme.panel_layoutbox = "theme.color.7"
 
 -- WALLPAPER:
---
--- Use plain color:
---theme.wallpaper_cmd     = "hsetroot"
 -- Use nitrogen:
 theme.wallpaper_cmd     = "nitrogen --restore"
 -- Use wallpaper tile:
@@ -32,56 +29,36 @@ theme.wallpaper_cmd     = "nitrogen --restore"
 --
 theme.show_widget_icon = false
 theme.use_iconfont = true
+--theme.widget_decoration_arrl = ''
+--theme.widget_decoration_arrr = ''
 theme.widget_decoration_arrl = ''
 theme.widget_decoration_arrr = ''
 
 -- FONTS:
---
---theme.font = "Source Code Pro Bold 10.5"
---theme.sans_font = "Source Sans Pro Bold 10.3"
---
---theme.font = "Meslo LG S for Lcarsline Bold 10.5" -- 10.7
---theme.sans_font = "PT Sans Bold 10.3"
---
--- use ~/.fonts.conf, Luke ;)
-
---infinality:
---theme.font = "Monospace Bold 10.6" -- pt mono
---theme.font = "Monospace Bold 11.2" -- ubuntu mono
---theme.font = "Monospace Bold 10.7" -- meslo lg s
---theme.sans_font = "Sans Bold 10.3" -- ubuntu sans
-
-theme.font = "Monospace Bold "..tostring(dpi(8)) -- meslo lg s
-theme.sans_font = "Sans Bold "..tostring(dpi(8)) -- ubuntu sans
-
+theme.font = "Monospace Bold "..tostring(dpi(9)) -- meslo lg s
+theme.sans_font = "Sans Bold "..tostring(dpi(9)) -- ubuntu sans
 -- Don't use sans font:
---theme.sans_font	= "theme.font"
+theme.sans_font	= "theme.font"
 
 --
 --MISC:
 --
 
-theme.taglist_squares_sel       = nil
-theme.taglist_squares_unsel     = nil
+theme.panel_padding_bottom = 6
+theme.panel_height = 18 + theme.panel_padding_bottom
+
+theme.border_width = 6
+theme.titlebar_height = 18 + theme.border_width
+
+
+theme.taglist_squares_sel       = "theme.null"
+theme.taglist_squares_unsel     = "theme.null"
 
 theme.titlebar_fg_focus		= "theme.titlebar_border"
 theme.titlebar_bg_focus		= "theme.titlebar_focus_border"
 theme.titlebar_fg_normal	= "theme.tasklist_fg_normal"
 theme.titlebar_bg_normal	= "theme.titlebar_border"
 
-theme.border_width = 6
-theme.titlebar_height = 24
-
---131dpi:
--- {{{
---theme.font = "Meslo LG S Bold 10.2"
---theme.font = "Monospace Bold 12" -- Fantasque Sans Mono
---theme.sans_font = "Ubuntu Sans Bold 10.3"
---theme.sans_font = "Sans Bold 9.6" -- Ubuntu Sans
---theme.sans_font      = "theme.font"
---theme.panel_height             = 26
---theme.panel_padding_bottom     = 6
--- }}}
 
 return create_theme({
   theme_name=theme_name,
