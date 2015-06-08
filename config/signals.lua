a45acb9295c55de17b54c4e1ebfe6da37290446c
actionless <actionless.loveless@gmail.com>
2015-06-08 16:35:54 +0200

    config: signals: add maximimize handler for lcars ; clean-up, improved debugging

--- config/signals.lua
+++ config/signals.lua
@@ -7,6 +7,8 @@ local titlebar	= require("actionless.titlebar")
 local lain = require("third_party.lain")
 local helpers = require("actionless.helpers")
 
+local delayed_call = require("gears.timer").delayed_call
+
 
 local signals = {}
 
@@ -16,18 +18,22 @@ local function on_client_focus(c)
   local layout = awful.layout.get(c.screen)
   if awesome_context.show_titlebar then
     -- titlebars enabled explicitly
+    --nlog(" titlebars enabled explicitly")
     c.border_color = beautiful.border_focus
     c.border_width = beautiful.border_width
     titlebar.make_titlebar(c)
   elseif c.maximized then
     -- maximized
+    --nlog("maximized")
     titlebar.remove_border(c)
   elseif awful.client.floating.get(c) then
     -- floating client
+    --nlog("-- floating client")
     c.border_width = beautiful.border_width
     titlebar.make_titlebar(c)
   elseif layout == awful.layout.suit.floating then
     -- floating layout
+    --nlog("floating layout")
     c.border_width = beautiful.border_width
     titlebar.make_titlebar(c)
   elseif #awful.client.tiled(c.screen) == 1 and not (
@@ -35,9 +41,11 @@ local function on_client_focus(c)
     or layout == lain.layout.uselesstile
   ) then
     -- one tiling client
+    --nlog("-- one tiling client")
     titlebar.remove_border(c)
   else
     -- more tiling clients
+    --nlog("-- more tiling clients")
     c.border_width = beautiful.border_width
     c.border_color = beautiful.border_focus
     titlebar.remove_titlebar(c)
@@ -107,7 +115,9 @@ tag.connect_signal("property::layout", function (t)
 end)
 
 client.connect_signal("property::maximized", function (c)
-  return on_client_focus(c)
+  delayed_call(function()
+    return on_client_focus(c)
+  end)
 end)
 
 client.connect_signal("property::minimized", function (c)
@@ -121,8 +131,9 @@ end)
 
 ----------------------------------------
 
-local function lcars_unite(t)
+local function lcars_unite(t, from)
   if not awesome_context.lcars_is_separated then return end
+  --nlog("unite|"..from)
   local s = helpers.get_current_screen()
   local w = awesome_context.topwibox[s]
   w:struts({top = beautiful.panel_height})
@@ -142,12 +153,13 @@ local function lcars_unite(t)
   awesome_context.lcars_is_separated = false
 end
 
-local function lcars_separate(t)
+local function lcars_separate(t, from)
   local s = awful.tag.getscreen(t)
   local nmaster = awful.tag.getnmaster(t)
   if nmaster < 1 or #awful.client.tiled(s) <= nmaster then
-    return lcars_unite(t)
+    return lcars_unite(t, from)
   end
+  --nlog("separate|"..from)
   local w = awesome_context.topwibox[s]
   if not awesome_context.lcars_is_separated then
     w:struts({top = 0})
@@ -160,6 +172,7 @@ local function lcars_separate(t)
   if awesome_context.lcars_is_separated
     and computed_y == awesome_context.lcars_last_y
   then return end
+  --nlog("--not cached")
   awesome_context.lcars_last_y = computed_y
 
   w:geometry({height = beautiful.panel_height * 2 + beautiful.panel_padding_bottom})
@@ -198,57 +211,55 @@ local function lcars_separate(t)
 end
 
 
-local function tag_callback(t)
+local function tag_callback(t, from)
+  if not t then t = awful.tag.selected(helpers.get_current_screen()) end
+
+  if not t.selected then return end
   if awful.tag.getproperty(t, 'layout').name == 'lcars' then
-    lcars_separate(t)
+    lcars_separate(t, from)
   else
-    lcars_unite(t)
+    lcars_unite(t, from)
   end
-end
-local function client_callback(c)
-  local t = awful.tag.selected(helpers.get_current_screen())
-  return tag_callback(t)
 end
 
-client.connect_signal("unmanage", function (c)
-  return client_callback(c)
+client.connect_signal("tagged", function (c, t)
+  return tag_callback(t, "c:tagged on " .. t.name)
 end)
-client.connect_signal("tagged", function (c)
-  for _, t in ipairs(c:tags()) do
-    if awful.tag.getproperty(t, 'layout').name == 'lcars' then
-      lcars_separate(t)
-    end
-  end
-end)
-client.connect_signal("untagged", function (c)
-  for _, t in ipairs(c:tags()) do
-    if awful.tag.getproperty(t, 'layout').name == 'lcars' then
-      lcars_separate(t)
-    end
-  end
+client.connect_signal("untagged", function (c, t)
+  return tag_callback(t, "c:untagged")
 end)
 client.connect_signal("property::minimized", function (c)
-  return client_callback(c)
+  return tag_callback(c:tags()[1], "c:min")
 end)
 
 tag.connect_signal("property::layout", function (t)
-  return tag_callback(t)
+  return tag_callback(t, "t:layout")
 end)
 tag.connect_signal("property::selected", function (t)
-  return tag_callback(t)
+  if t.selected then 
+    return tag_callback(t, "t:selected")
+  end
 end)
 tag.connect_signal("property::mwfact", function (t)
   if awful.tag.getproperty(t, 'layout').name == 'lcars' then
-    lcars_separate(t)
+    lcars_separate(t, "t:mwfact")
   end
 end)
 tag.connect_signal("property::ncol", function (t)
-  return tag_callback(t)
+  return tag_callback(t, "t:ncol")
 end)
 tag.connect_signal("property::nmaster", function (t)
-  return tag_callback(t)
+  return tag_callback(t, "t:nmaster")
 end)
 
+client.connect_signal("property::maximized", function (c)
+      awesome_context.left_panel_visible = not awesome_context.left_panel_visible
+      --awesome_context.leftwibox[1]:struts({left=0})
+      awesome_context.leftwibox[1].visible = awesome_context.left_panel_visible
+      awesome_context.internal_corner_wibox[1].visible = awesome_context.left_panel_visible
+      awful.wibox.stretch(awesome_context.topwibox[1], 1)
+      c:geometry({width=screen[c.screen].workarea.width})
+end)
 
 end
 -- }}}
