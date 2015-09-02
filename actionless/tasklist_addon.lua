6a1ed874010ce9c0762ab2eebac96704a6766b7d
actionless <actionless.loveless@gmail.com>
2015-09-03 00:13:16 +0200

    fix(tasklist_addon): work with exposed tb

--- actionless/tasklist_addon.lua
+++ actionless/tasklist_addon.lua
@@ -67,7 +67,7 @@ function tasklist_addon.list_update(w, buttons, label, data, objects, spacing_le
             }
         end
 
-        local text, bg, bg_image, icon = label(o)
+        local text, bg, bg_image, icon = label(o, tb)
         -- The text might be invalid, so use pcall
         if not pcall(tb.set_markup, tb, text) then
             tb:set_markup("<i>&lt;Invalid text&gt;</i>")
