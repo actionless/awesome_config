60b90a32fb7e21da8306fc6d73b46b1b6abef2b5
actionless <actionless.loveless@gmail.com>
2016-02-12 03:09:35 +0100

    theme(lcars-xresources): move one pixel from gap to border

--- themes/lcars-xresources-hidpi/theme.lua
+++ themes/lcars-xresources-hidpi/theme.lua
@@ -78,9 +78,13 @@ theme.sans_font = "Sans Bold 10"
 
 theme.basic_panel_height = dpi(18)
 theme.panel_padding_bottom = dpi(3)
-theme.border_width = dpi(3)
---theme.useless_gap = dpi(10)
-theme.useless_gap = dpi(6)
+
+--theme.border_width = dpi(3)
+--theme.useless_gap = dpi(6)
+
+theme.border_width = dpi(4)
+theme.useless_gap = dpi(5)
+
 theme.panel_height = theme.basic_panel_height + theme.panel_padding_bottom
 theme.titlebar_height = theme.basic_panel_height + theme.border_width
 
