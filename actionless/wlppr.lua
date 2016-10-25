0313260740606649b5862fbedfe40604628f5ee5
actionless <actionless.loveless@gmail.com>
2016-10-25 15:41:59 +0200

    feat(wlppr): don't do lock on client side

--- actionless/wlppr.lua
+++ actionless/wlppr.lua
@@ -2,37 +2,29 @@ local awful = require("awful")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 
-
 local wlppr = {}
 
-wlppr.lock = false
 
 function wlppr.load_new()
-  if wlppr.lock then
-    log("WALLPAPER LOCKED (in progress)")
-    return
-  end
-  wlppr.lock = true
   awful.spawn.easy_async(
     '/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/load_images.py',
-    function(...)
-      wlppr.lock = false
-      log(table.pack(...))
+    function(stderr, stdout, reason, code)
+      print(' \n')
+      print(stdout)
+      print(stderr)
+      print(' \n')
     end
   )
 end
 
 function wlppr.cycle()
-  if wlppr.lock then
-    log("WALLPAPER LOCKED (in progress)")
-    return
-  end
-  wlppr.lock = true
   awful.spawn.easy_async(
     'bash -c "nitrogen --set-color='..(beautiful.gtk and beautiful.gtk.BG or beautiful.xrdb.background)..' --set-tiled $(/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/save_random_image.py)"',
-    function(...)
-      wlppr.lock = false
-      log(table.pack(...))
+    function(stderr, stdout, reason, code)
+      print(' \n')
+      print(stdout)
+      print(stderr)
+      print(' \n')
     end
   )
 end
@@ -49,8 +41,9 @@ end
 function wlppr.cycle_best()
   awful.spawn.easy_async(
     'bash -c "nitrogen --set-color='..(beautiful.gtk and beautiful.gtk.BG or beautiful.xrdb.background)..' --set-tiled $(/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/get_best.py)"',
-    function(...)
-      log(table.pack(...))
+    function(stderr, stdout, reason, code)
+      print(stdout)
+      print(stderr)
     end
   )
 end
@@ -58,10 +51,37 @@ end
 function wlppr.save()
   awful.spawn.easy_async(
     'bash -c "cp $(/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/get_last_path.py)* /home/lie/projects/tumblr/image_log/best/"',
-    function(...)
-      log(table.pack(...))
+    function(stderr, stdout, reason, code)
+      print(stdout)
+      print(stderr)
       naughty.notify({
-        text = 'saved',
+        text = 'saved'..stderr..stdout,
+      })
+    end
+  )
+end
+
+function wlppr.save_best()
+  awful.spawn.easy_async(
+    'bash -c "cp $(/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/get_last_path.py)* \\"/home/lie/projects/tumblr/image_log/the very best/\\""',
+    function(stderr, stdout, reason, code)
+      print(stdout)
+      print(stderr)
+      naughty.notify({
+        text = 'saved to best'..stderr..stdout,
+      })
+    end
+  )
+end
+
+function wlppr.dump()
+  awful.spawn.easy_async(
+    'bash -c "mv $(/home/lie/projects/tumblr/env/bin/python /home/lie/projects/tumblr/get_last_path.py)* \"/home/lie/projects/tumblr/image_log/dump/\""',
+    function(stderr, stdout, reason, code)
+      print(stdout)
+      print(stderr)
+      naughty.notify({
+        text = 'dumped'..stderr..stdout,
       })
     end
   )
