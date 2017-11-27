de4e04e9695584caa4d381fc3631a5f2a3d1943e
actionless <actionless.loveless@gmail.com>
2017-11-27 01:05:03 +0100

    feat(config: menus): show menu instantly and add freedesktop menu items later on

--- config/menus.lua
+++ config/menus.lua
@@ -1,16 +1,16 @@
 local awful = require("awful")
-local awesome = awesome
---local menubar = require("menubar")
-local menubar = require("actionless.menubar")
+local awesome_menubar = require("menubar")
 local beautiful = require("beautiful")
-local capi = { screen = screen }
 
+local menubar = require("actionless.menubar")
 local menugen = require("utils.menugen")
 local wlppr = require("actionless.wlppr")
 
 local menus = {}
 
 
+-- Menu
+-- Create a laucher widget and a main menu
 function menus.init(context)
 
   local applications_menu = {
@@ -30,8 +30,6 @@ function menus.init(context)
     { "terminal", "xterm" }
   }
 
-  -- {{{ Menu
-  -- Create a laucher widget and a main menu
   local myawesomemenu = {
     { "manual", context.cmds.terminal .. " -e man awesome" },
     { "edit config", context.cmds.editor_cmd .. " " .. awesome.conffile },
@@ -42,6 +40,38 @@ function menus.init(context)
     { "poweroff", "poweroff" },
   }
 
+  local menu_content = {
+    { "freedesktop loading...", nil, nil },
+    { "wlppr",
+      {{
+        "save", wlppr.save,
+        beautiful.widget_ac_charging_low
+      }, {
+        "save to best", wlppr.save_best,
+        beautiful.widget_ac_charging
+      }, {
+        "dump", wlppr.dump,
+        beautiful.widget_music_stop
+      }},
+      beautiful.widget_hdd
+    },
+    { "awesome", myawesomemenu, beautiful.awesome_icon },
+    { "jack",
+      {{
+        "start", os.getenv("HOME").."/scripts/jack_start.sh",
+        beautiful.widget_music_pause
+      }, {
+        "stop", os.getenv("HOME").."/scripts/jack_stop.sh",
+        beautiful.widget_music_stop
+      }},
+      beautiful.widget_vol
+    },
+    { "applications", applications_menu, beautiful.applications_icon },
+    --{ "kill compositor", "killall compton" },
+    --{ "start compositor", context.cmds.compositor },
+    { "open terminal", context.cmds.terminal }
+  }
+
   function context.menu.mainmenu_show(nomouse)
     local function show_menu()
       local args = {}
@@ -49,41 +79,15 @@ function menus.init(context)
       context.menu.mainmenu:show(args)
     end
     if not context.menu.mainmenu then
+      context.menu.mainmenu = awful.menu({
+        items =menu_content,
+      })
+      show_menu()
       menugen.build_menu(function(menulist)
-        context.menu.mainmenu = awful.menu({
-          items = {
-            { "wlppr",
-              {{
-                "save", wlppr.save,
-                beautiful.widget_ac_charging_low
-              }, {
-                "save to best", wlppr.save_best,
-                beautiful.widget_ac_charging
-              }, {
-                "dump", wlppr.dump,
-                beautiful.widget_music_stop
-              }},
-              beautiful.widget_hdd
-            },
-            { "freedesktop", menulist, beautiful.awesome_icon },
-            { "awesome", myawesomemenu, beautiful.awesome_icon },
-            { "jack",
-              {{
-                "start", os.getenv("HOME").."/scripts/jack_start.sh",
-                beautiful.widget_music_pause
-              }, {
-                "stop", os.getenv("HOME").."/scripts/jack_stop.sh",
-                beautiful.widget_music_stop
-              }},
-              beautiful.widget_vol
-            },
-            { "applications", applications_menu, beautiful.applications_icon },
-            --{ "kill compositor", "killall compton" },
-            --{ "start compositor", context.cmds.compositor },
-            { "open terminal", context.cmds.terminal }
-          },
-        })
-        show_menu()
+        context.menu.mainmenu:delete(1)
+        context.menu.mainmenu:add(
+          {"freedesktop", menulist, beautiful.awesome_icon}, 1
+        )
       end)
     else
       show_menu()
@@ -96,26 +100,27 @@ function menus.init(context)
       return context.menu.mainmenu:toggle()
     end
   end
-  -- }}}
-  --
 
+  -- Menubar configuration
+  awesome_menubar.utils.terminal = context.cmds.terminal
+  context.menu.menubar = menubar.create()
+  -- @TODO: for some reason this is crashing
+  --context.menu.menubar = awesome_menubar.get()
+
+  -- D-Menubar configuration
   menubar.utils.terminal = context.cmds.terminal
   menubar.geometry = {
     height = beautiful.panel_height,
-    width = capi.screen[awful.screen.focused()].workarea.width,
+    width = screen[awful.screen.focused()].workarea.width,
     x = 0,
-    y = capi.screen[awful.screen.focused()].workarea.height - beautiful.panel_height
+    y = screen[awful.screen.focused()].workarea.height - beautiful.panel_height
   }
-  -- Menubar configuration
-  context.menu.menubar = menubar.create()
-  -- D-Menubar configuration
   context.menu.dmenubar = menubar.create({
     term_prefix = context.cmds.tmux_run,
   })
   context.menu.dmenubar.cache_entries = false
   context.menu.dmenubar.menu_cache_path = awful.util.getdir("cache") .. "/history"
   context.menu.dmenubar.menu_gen = require("actionless.menubar.dmenugen")
-  -- }}}
 
 end
 
