bdf0eee0372bc23653a05989c03432725588de45
actionless <actionless.loveless@gmail.com>
2017-12-06 03:55:23 +0100

    fix(config: signals): compute inner border radius based on border_width instead of magic number - 3

--- config/signals.lua
+++ config/signals.lua
@@ -237,9 +237,9 @@ local function round_up_client_corners(c, force, reference)
     if client_tag.layout.name == "floating" or client_tag:get_gap() ~= 0 then
       outer_shape_args = beautiful.client_border_radius
     end
-    --local inner_shape_args = beautiful.client_border_radius*0.75
-    local inner_shape_args = beautiful.client_border_radius - beautiful.base_border_width
-    if inner_shape_args < 0 then inner_shape_args = 0 end
+    local inner_shape_args = beautiful.client_border_radius*0.75
+    --local inner_shape_args = beautiful.client_border_radius - beautiful.base_border_width
+    --if inner_shape_args < 0 then inner_shape_args = 0 end
     apply_shape(c, gears.shape.rounded_rect, outer_shape_args, inner_shape_args)
     clog("apply_shape "..(reference or 'no_ref'), c)
     pending_shapes[c] = nil
