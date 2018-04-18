0e388c790414b45fe1dbc096c2cdb68ae7419998
actionless <actionless.loveless@gmail.com>
2018-04-18 04:54:25 +0200

    feat(widgets: music): add `gradio` backend

--- actionless/widgets/music/backends/init.lua
+++ actionless/widgets/music/backends/init.lua
@@ -3,6 +3,7 @@ local backends = {
   spotify	= require("actionless.widgets.music.backends.spotify"),
   mpd	= require("actionless.widgets.music.backends.mpd"),
   mopidy = require("actionless.widgets.music.backends.mopidy"),
+  gradio = require("actionless.widgets.music.backends.gradio"),
 }
 
 return backends
