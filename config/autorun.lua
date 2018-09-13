13928494b26faaaec90267f69a99995c3dfb897b
actionless <actionless.loveless@gmail.com>
2018-09-13 05:56:22 +0200

    chore(autorun): use named xinput example

--- config/autorun.lua
+++ config/autorun.lua
@@ -9,7 +9,7 @@ function autorun.init(awesome_context)
 
   awful.spawn.with_shell('pgrep vmtoolsd >/dev/null && /usr/bin/vmware-user-suid-wrapper')
   --awful_spawn.with_shell(os.getenv('HOME').."/.screenlayout/awesome.sh")
-  --awful.spawn.with_shell('xinput disable 11')
+  --awful.spawn.with_shell('xinput disable "ELAN Touchscreen"')
 
   --local kensinton =12
   local kensington = nil
