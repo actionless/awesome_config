2282e4e66dd9ab35ff23a263bb52576903094dc7
actionless <actionless.loveless@gmail.com>
2018-11-12 07:25:52 +0100

    refactor(widgets: keyboardlayout): fix following panel shape and clean-up variable names

--- actionless/widgets/kbd.lua
+++ actionless/widgets/kbd.lua
@@ -3,33 +3,33 @@ Licensed under GNU General Public License v2
 * (c) 2013-2014, Yauheni Kirylau
 --]]
 
+local wibox = require('wibox')
+local awful_widget_keyboardlayout = require("awful.widget.keyboardlayout")
+
 local capi = {
   awesome=awesome
 }
-local awesome_keyboardlayout = require("awful.widget.keyboardlayout")
 
 local common = require("actionless.widgets.common")
 
 local kbd = {}
-kbd.widget = common.widget()
 
-local function worker(args)
+local function widget_factory(args)
   args	 = args or {}
   args.orientation = args.orientation or "horizontal"
-  args.widget=kbd.widget
-  kbd.widget = common.decorated(args)
-  kbd.widget:hide()
-
-  local awesome_keayboardlayout_instance = awesome_keyboardlayout()
   local replacements = args.replacements or {us="eng", ["ru(winkeys)"]="rus"}
   local default_layout = args.default_layout or "us"
 
+  args.widget = wibox.widget.textbox()
+  kbd.widget = common.panel_shape(common.decorated(args))
+
+  local akb_widget = awful_widget_keyboardlayout()
+
   local function update_status()
-    local self =  awesome_keayboardlayout_instance
-    self._current = capi.awesome.xkb_get_layout_group()
+    akb_widget._current = capi.awesome.xkb_get_layout_group()
     local text = ""
-    if (#self._layout > 0) then
-        text = (self._layout[self._current+1])
+    if (#akb_widget._layout > 0) then
+        text = (akb_widget._layout[akb_widget._current+1])
     end
     if text == default_layout then
       kbd.widget:hide()
@@ -40,12 +40,13 @@ local function worker(args)
   end
 
   update_status()
-  capi.awesome.connect_signal("xkb::group_changed",
-    function () update_status() end);
-  capi.awesome.connect_signal("xkb::map_changed",
-    function () update_status() end)
+
+  capi.awesome.connect_signal("xkb::group_changed", update_status)
+  capi.awesome.connect_signal("xkb::map_changed", update_status)
 
   return setmetatable(kbd, { __index = kbd.widget })
 end
 
-return setmetatable(kbd, { __call = function(_, ...) return worker(...) end })
+return setmetatable(kbd, { __call = function(_, ...)
+  return widget_factory(...)
+end })
