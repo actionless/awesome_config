a47dbe00cfacde15784f6d5fdf28de1bbf62ff35
actionless <actionless.loveless@gmail.com>
2019-01-15 04:30:01 +0100

    feat(config: menu): use more icons and beautiful.icon_theme

--- config/menu.lua
+++ config/menu.lua
@@ -1,17 +1,50 @@
-local awful = require("awful")
+local awful_menu = require("awful.menu")
+local awful_spawn = require("awful.spawn")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
 local awesome_menubar = require("menubar")
 local beautiful = require("beautiful")
+local gfs = require("gears.filesystem")
+
 
 local menugen = require("actionless.util.menugen")
 local wlppr = require("actionless.wlppr")
 
+
+local ICON_SIZES = {
+  ['256x256']='png',
+  ['128x128']='png',
+  ['scalable']='svg',
+  ['32x32']='png',
+}
+local ICON_THEMES = {
+  beautiful.icon_theme,
+  'gnome',
+}
+
+
 local menus = {}
 
-local ICON_SIZE = 256
 
 local function get_icon(category, name)
-  return "/usr/share/icons/gnome/"..ICON_SIZE.."x"..ICON_SIZE.."/"..category.."/"..name..".png"
+  for _, icon_theme_name in ipairs(ICON_THEMES) do
+    for _, icon_root in ipairs({
+      os.getenv('HOME') .. '/.icons/',
+      '/usr/share/icons/',
+    }) do
+      for icon_size, extension in pairs(ICON_SIZES) do
+        for _, path in ipairs({
+          icon_root .. icon_theme_name .. "/" .. icon_size .. "/" .. category .. "/" .. name .. "." .. extension,
+          icon_root .. icon_theme_name .. "/" .. category .. "/" .. icon_size .. "/" .. name .. "." .. extension,
+        }) do
+          if gfs.file_readable(path) then
+            return path
+          end
+        end
+      end
+    end
+  end
 end
+
 
 -- Menu
 -- Create a laucher widget and a main menu
@@ -29,29 +62,35 @@ function menus.init(context)
 
   local function logout()
     kill_everybody()
-    --awful.spawn('mate-session-save --gui --logout-dialog')
-    awful.spawn('qdbus --literal org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout 1')
+    --awful_spawn('mate-session-save --gui --logout-dialog')
+    awful_spawn('qdbus --literal org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.Logout 1')
   end
 
   local function poweroff()
     kill_everybody()
-    --awful.spawn('mate-session-save --gui --shutdown-dialog')
-    awful.spawn('qdbus org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RequestShutdown')
+    --awful_spawn('mate-session-save --gui --shutdown-dialog')
+    awful_spawn('qdbus org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RequestShutdown')
   end
 
   local function reboot()
     kill_everybody()
-    --awful.spawn('mate-session-save --gui --shutdown-dialog')
-    awful.spawn('qdbus org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RequestReboot')
+    --awful_spawn('mate-session-save --gui --shutdown-dialog')
+    awful_spawn('qdbus org.gnome.SessionManager /org/gnome/SessionManager org.gnome.SessionManager.RequestReboot')
   end
 
+
   local myawesomemenu = {
+    { "hotkeys", function() return false, hotkeys_popup.show_help end},
     { "manual page", awesome_menubar.utils.terminal .. " -e man awesome" },
     { "edit config", context.cmds.editor_cmd .. " " .. awesome.conffile },
     { "reload", awesome.restart },
-    { "quit", function() awesome.quit() end},
-    { "quit2 (toggle argb)", function() awesome.quit(2) end},
-    { "quit3 (openbox)", function() awesome.quit(3) end},
+
+    -- Without X Session Manager:
+    --{ "quit", function() awesome.quit() end},
+    --{ "quit2 (toggle argb)", function() awesome.quit(2) end},
+    --{ "quit3 (openbox)", function() awesome.quit(3) end},
+
+    -- With X Session Manager:
     { "logout", logout, get_icon('actions', 'system-log-out') },
     { "reboot", reboot, get_icon('actions', 'view-refresh') },
     { "poweroff", poweroff, get_icon('actions', 'system-shutdown') },
@@ -63,25 +102,29 @@ function menus.init(context)
       { "PTIPython", term .. "ptipython" },
     }},
     { "Graphics", {
-      { "Viewnior", "viewnior" },
-      { "Nomacs",   "nomacs" },
       { "GIMP",     "gimp" },
+      { "Nomacs",   "nomacs" },
+      { "Viewnior", "viewnior" },
     }},
     { "Multimedia", {
       { "Clementine", "clementine" },
       { "GRadio", "gradio" },
       { "mpv", "mpv" },
     }},
+    { "Settings", {
+      { "dconf Editor", "dconf-editor" },
+    }},
     { "Terminals", {
-      { "XTerm", "xterm" },
-      { "URxvt", "urxvt" },
       { "st", "st" },
+      { "tmux in xst", "xst-tmux" },
+      { "URxvt", "urxvt" },
       { "xst", "xst" },
+      { "XTerm", "xterm" },
     }},
     { "Text", {
-      { "mEdit", "medit" },
       --{ "Geany", "geany" },
       { "ghostwriter", "ghostwriter" },  -- or retext?
+      { "mEdit", "medit" },
       { "Oni", "oni" },
       { "retext", "retext" },
       --{ "vnote", "vnote" },  -- replace to mindforger?
@@ -89,34 +132,43 @@ function menus.init(context)
   }
 
   local menu_content = {
-    { "freedesktop loading...", nil, nil },
-    { "open terminal", awesome_menubar.utils.terminal },
+    {
+      "freedesktop loading...", nil,
+      get_icon('status', 'image-loading')
+    },
+    {
+      "open terminal", awesome_menubar.utils.terminal,
+      get_icon('apps', 'terminal')
+    },
     --{ "kill compositor", "killall compton" },
     --{ "start compositor", context.cmds.compositor },
-    { "wlppr",
+    {
+      "wlppr",
       {{
         "save", wlppr.save,
-        beautiful.widget_ac_charging_low
+        get_icon('actions', 'filesave')
       }, {
         "save to best", wlppr.save_best,
-        beautiful.widget_ac_charging
+        get_icon('status', 'starred')
       }, {
         "dump", wlppr.dump,
-        beautiful.widget_music_stop
+        get_icon('status', 'trashcan_full')
       }},
-      beautiful.widget_hdd
+      get_icon('apps', 'wallpaper')
     },
-    { "jack",
+    {
+      "jack",
       {{
         "start", os.getenv("HOME").."/scripts/jack_start.sh",
-        beautiful.widget_music_pause
+        get_icon('status', 'audio-volume-high')
       }, {
         "stop", os.getenv("HOME").."/scripts/jack_stop.sh",
-        beautiful.widget_music_stop
+        get_icon('actions', 'stop')
       }},
-      beautiful.widget_vol
+      --get_icon('devices', 'audio-input-microphone')
+      get_icon('apps', 'audio-player')
     },
-    { "applications", applications_menu, beautiful.applications_icon },
+    { "applications", applications_menu, get_icon('apps', 'menu-editor') },
     { "awesome", myawesomemenu, beautiful.awesome_icon },
   }
 
@@ -128,7 +180,7 @@ function menus.init(context)
       context.menu.mainmenu:show(args)
     end
     if not context.menu.mainmenu then
-      context.menu.mainmenu = awful.menu({
+      context.menu.mainmenu = awful_menu({
         items =menu_content,
       })
       show_menu()
