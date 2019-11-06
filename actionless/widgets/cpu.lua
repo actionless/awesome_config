26dfde2c893913a3e7eb5bb468e5e18dc18067c7
actionless <actionless.loveless@gmail.com>
2019-11-06 22:17:18 +0100

    refactor(widgets: cpu): nicer structure

--- actionless/widgets/cpu.lua
+++ actionless/widgets/cpu.lua
@@ -23,12 +23,142 @@ local cpu = {
   notification = nil,
 }
 
-local function worker(args)
+function cpu.hide_notification()
+  if cpu.notification ~= nil then
+    naughty.destroy(cpu.notification)
+    cpu.notification = nil
+  end
+end
+
+function cpu.get_notification_id()
+  return cpu.notification and cpu.notification.id or nil
+end
+
+function cpu.show_notification()
+  cpu.notification = naughty.notify({
+    text = "waiting for top...",
+    timeout = cpu.timeout,
+    font = beautiful.mono_font,
+    replaces_id = cpu.get_notification_id(),
+    position = beautiful.widget_notification_position,
+  })
+  awful.spawn.easy_async(
+    cpu.command,
+    cpu.notification_callback_done
+  )
+  cpu.update()
+end
+
+function cpu.notification_callback_done(output)
+  local notification_id = cpu.get_notification_id()
+  if not notification_id then return end
+  local result = {}
+  local names = {}
+  local column_headers = h_string.split(
+    h_table.range(
+      parse.string_to_lines(output),
+      6, 6
+    )[1], ' '
+  )
+  for _, line in ipairs(
+    h_table.range(
+      parse.string_to_lines(output),
+      7
+    )
+  ) do
+    --local pid, percent, name = line:match("^(%d+)%s+(.+)%s+(.*)")
+    local values = h_string.split(line, ' ')
+    local pid = values[cpu.columns.pid]
+    local percent = values[cpu.columns.percent]
+    local name = values[cpu.columns.name]
+    percent = percent + 0
+    if percent and percent ~= 0 and name ~= 'top' then
+      if result[pid] then
+        result[pid] = (result[pid] + percent)/2
+      elseif name then
+        result[pid] = percent
+      end
+      names[pid] = name
+    end
+  end
+
+  local pid_width = 7
+
+  local result_string = ''
+  local counter = 0
+  local num_records = h_table.getn(result)
+  for pid, percent in h_table.spairs(result, function(t,a,b) return t[b] < t[a] end) do
+    if cpu.show_pid then
+      result_string = result_string .. string.format("%"..pid_width.."s %6.2f %s", pid, percent, names[pid])
+    else
+      result_string = result_string .. string.format("%6.2f %s", percent, names[pid])
+    end
+    counter = counter + 1
+    if counter == cpu.list_len or counter == num_records then
+      break
+    end
+    result_string = result_string .. '\n'
+  end
+
+  if result_string == '' then
+    result_string = "no running processes atm"
+  else
+    local header_string
+    if cpu.show_pid then
+      header_string = string.format(
+        '%'..pid_width..'s %6s %s\n',
+        column_headers[cpu.columns.pid],
+        column_headers[cpu.columns.percent],
+        column_headers[cpu.columns.name]
+      )
+    else
+      header_string = string.format(
+        '%6s %s\n',
+        column_headers[cpu.columns.percent],
+        column_headers[cpu.columns.name]
+      )
+    end
+    result_string = (header_string ..
+      '<span font="'  .. tostring(beautiful.text_font)  .. '">' .. result_string .. '</span> '
+    )
+  end
+
+  cpu.notification = naughty.notify({
+    text = result_string,
+    timeout = cpu.timeout,
+    font = beautiful.mono_font,
+    replaces_id = cpu.get_notification_id(),
+    position = beautiful.widget_notification_position,
+  })
+end
+
+function cpu.update()
+  cpu.now.la1, cpu.now.la5, cpu.now.la15 = parse.find_in_file(
+    "/proc/loadavg",
+    "^([0-9.]+) ([0-9.]+) ([0-9.]+) .*"
+  )
+  local msg = string.format(
+    "%-4s", cpu.now.la1
+  )
+  if tonumber(cpu.now.la1) > cpu.cores_number * 2 then
+    cpu.widget:set_error()
+  elseif tonumber(cpu.now.la1) > cpu.cores_number then
+    cpu.widget:set_warning()
+  else
+    cpu.widget:set_normal()
+    msg = 'cpu'
+  end
+  cpu.widget.textbox:set_text(msg)
+  cpu.widget.progressbar:set_value(cpu.now.la1/cpu.cores_number)
+end
+
+function cpu.init(args)
   args     = args or {}
   local update_interval  = args.update_interval or 5
   cpu.cores_number = tonumber(parse.command_to_string('nproc'))
   cpu.timeout = args.timeout or 0
   cpu.show_pid = args.show_pid or false
+  cpu.list_len = args.list_length or 10
 
   local widget = common_widgets.text_progressbar(args)
   cpu.widget = common_widgets.decorated{widget=widget}
@@ -37,11 +167,11 @@ local function worker(args)
 
   --cpu.widget:set_image(beautiful.widget_cpu)
   cpu.widget:connect_signal(
-    "mouse::enter", function () cpu.show_notification() end)
+    "mouse::enter", function () cpu.show_notification() end
+  )
   cpu.widget:connect_signal(
-    "mouse::leave", function () cpu.hide_notification() end)
-
-  cpu.list_len = args.list_length or 10
+    "mouse::leave", function () cpu.hide_notification() end
+  )
 
   cpu.command = "top -o \\%CPU -b -n 1 -w 512"
   cpu.columns = args.columns or {
@@ -49,135 +179,6 @@ local function worker(args)
     percent=9,
     name=12
   }
-
-  function cpu.hide_notification()
-    if cpu.notification ~= nil then
-      naughty.destroy(cpu.notification)
-      cpu.notification = nil
-    end
-  end
-
-  function cpu.get_notification_id()
-    return cpu.notification and cpu.notification.id or nil
-  end
-
-  function cpu.show_notification()
-    cpu.notification = naughty.notify({
-      text = "waiting for top...",
-      timeout = cpu.timeout,
-      font = beautiful.mono_font,
-      replaces_id = cpu.get_notification_id(),
-      position = beautiful.widget_notification_position,
-    })
-    awful.spawn.easy_async(
-      cpu.command,
-      cpu.notification_callback_done
-    )
-    cpu.update()
-  end
-
-  function cpu.notification_callback_done(output)
-    local notification_id = cpu.get_notification_id()
-    if not notification_id then return end
-    local result = {}
-    local names = {}
-    local column_headers = h_string.split(
-      h_table.range(
-        parse.string_to_lines(output),
-        6, 6
-      )[1], ' '
-    )
-    for _, line in ipairs(
-      h_table.range(
-        parse.string_to_lines(output),
-        7
-      )
-    ) do
-      --local pid, percent, name = line:match("^(%d+)%s+(.+)%s+(.*)")
-      local values = h_string.split(line, ' ')
-      local pid = values[cpu.columns.pid]
-      local percent = values[cpu.columns.percent]
-      local name = values[cpu.columns.name]
-      percent = percent + 0
-      if percent and percent ~= 0 and name ~= 'top' then
-        if result[pid] then
-          result[pid] = (result[pid] + percent)/2
-        elseif name then
-          result[pid] = percent
-        end
-        names[pid] = name
-      end
-    end
-
-    local pid_width = 7
-
-    local result_string = ''
-    local counter = 0
-    local num_records = h_table.getn(result)
-    for pid, percent in h_table.spairs(result, function(t,a,b) return t[b] < t[a] end) do
-      if cpu.show_pid then
-        result_string = result_string .. string.format("%"..pid_width.."s %6.2f %s", pid, percent, names[pid])
-      else
-        result_string = result_string .. string.format("%6.2f %s", percent, names[pid])
-      end
-      counter = counter + 1
-      if counter == cpu.list_len or counter == num_records then
-        break
-      end
-      result_string = result_string .. '\n'
-    end
-
-    if result_string == '' then
-      result_string = "no running processes atm"
-    else
-      local header_string
-      if cpu.show_pid then
-        header_string = string.format(
-          '%'..pid_width..'s %6s %s\n',
-          column_headers[cpu.columns.pid],
-          column_headers[cpu.columns.percent],
-          column_headers[cpu.columns.name]
-        )
-      else
-        header_string = string.format(
-          '%6s %s\n',
-          column_headers[cpu.columns.percent],
-          column_headers[cpu.columns.name]
-        )
-      end
-      result_string = (header_string ..
-        '<span font="'  .. tostring(beautiful.text_font)  .. '">' .. result_string .. '</span> '
-      )
-    end
-
-    cpu.notification = naughty.notify({
-      text = result_string,
-      timeout = cpu.timeout,
-      font = beautiful.mono_font,
-      replaces_id = cpu.get_notification_id(),
-      position = beautiful.widget_notification_position,
-    })
-  end
-
-  function cpu.update()
-    cpu.now.la1, cpu.now.la5, cpu.now.la15 = parse.find_in_file(
-      "/proc/loadavg",
-      "^([0-9.]+) ([0-9.]+) ([0-9.]+) .*"
-    )
-    local msg = string.format(
-      "%-4s", cpu.now.la1
-    )
-    if tonumber(cpu.now.la1) > cpu.cores_number * 2 then
-      cpu.widget:set_error()
-    elseif tonumber(cpu.now.la1) > cpu.cores_number then
-      cpu.widget:set_warning()
-    else
-      cpu.widget:set_normal()
-      msg = 'cpu'
-    end
-    cpu.widget.textbox:set_text(msg)
-    cpu.widget.progressbar:set_value(cpu.now.la1/cpu.cores_number)
-  end
 
   gears_timer({
     callback=cpu.update,
@@ -189,4 +190,4 @@ local function worker(args)
   return setmetatable(cpu, { __index = cpu.widget })
 end
 
-return setmetatable(cpu, { __call = function(_, ...) return worker(...) end })
+return setmetatable(cpu, { __call = function(_, ...) return cpu.init(...) end })
