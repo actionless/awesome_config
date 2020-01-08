4744e3f5b48cb156400eefa8a1cf31a6c85dce5b
actionless <actionless.loveless@gmail.com>
2020-01-08 05:03:47 +0100

    chore(themes, icons): add back `pokemon` + start replacing icons to more highres ones

---
+++ themes/pokemon/theme.lua
@@ -0,0 +1,30 @@
+local getdir = require("awful.util").getdir
+local g_string = require("gears.string")
+
+local create_theme = require("actionless.common_theme").create_theme
+
+
+local theme_dir = getdir("config").."/themes/pokemon/"
+local icons_dir = getdir("config").."/icons/pokemon_hidpi/"
+
+local theme = dofile(getdir("config").."/themes/gtk/theme.lua")
+-- remove all the icons:
+for full_name, value in pairs(theme) do
+  if (
+      type(value)=="string" and
+      g_string.startswith(value, theme.icons_dir) and
+      (value ~= theme.icons_dir)
+  ) then
+    theme[full_name] = nil
+  end
+end
+
+theme.dir = theme_dir
+theme.icons_dir = icons_dir
+
+theme.show_widget_icon = true
+theme.recolor_widget_icons = false
+
+theme = create_theme({ theme=theme, icons_dir=icons_dir, theme_dir=theme_dir })
+
+return theme
