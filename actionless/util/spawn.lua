18019472f800fac89b113a3a069baa26dc60a740
actionless <actionless.loveless@gmail.com>
2020-05-18 09:20:10 +0200

    doc(util: spawn): remove outdated license block as code related to it was removed long before

--- actionless/util/spawn.lua
+++ actionless/util/spawn.lua
@@ -1,9 +1,3 @@
---[[
-Licensed under GNU General Public License v2
-* (c) 2013,      Luke Bonham
-* (c) 2010-2012, Peter Hofmann
---]]
-
 local awful_spawn = require("awful.spawn")
 
 local spawn = {}
