f8f2d67817605e80714c3b3cc8aff4c2eb1d9121
actionless <actionless.loveless@gmail.com>
2020-06-30 00:12:16 +0200

    fix(widgets: music: spotify): workaround for broken coverart url

--- actionless/widgets/music/backends/spotify.lua
+++ actionless/widgets/music/backends/spotify.lua
@@ -5,4 +5,16 @@
 
 local mpris_creator = require("actionless.widgets.music.mpris_creator")
 
-return mpris_creator('spotify')
+local spotify = mpris_creator('spotify')
+
+local orig_parse_metadata = spotify.parse_metadata
+function spotify.parse_metadata(result, parse_status_callback)
+  -- Spotify client for Linux as usually sucks HUGE dicks:
+  -- https://community.spotify.com/t5/Desktop-Linux/MPRIS-cover-art-url-file-not-found/td-p/4920104
+  if result['mpris:artUrl'] ~= nil then
+    result['mpris:artUrl'] = result['mpris:artUrl']:gsub('https://open.spotify.com/image/', 'https://i.scdn.co/image/')
+  end
+  return orig_parse_metadata(result, parse_status_callback)
+end
+
+return spotify
