f5c2d496678b10ccd0e9213c5b65723c482f11d2
actionless <actionless.loveless@gmail.com>
2020-10-04 00:19:57 +0200

    feat(widgets: music: backends): add one for 'Shortwave'

---
+++ actionless/widgets/music/backends/shortwave.lua
@@ -0,0 +1,8 @@
+--[[
+  Licensed under GNU General Public License v2
+   * (c) 2020, Yauheni Kirylau
+--]]
+
+local mpris_creator = require("actionless.widgets.music.mpris_creator")
+
+return mpris_creator('de.haeckerfelix.Shortwave', {cmd='shortwave'})
