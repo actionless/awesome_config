876467845ebd292bb52aabc7b1b8739ba4e9ee89
actionless <actionless.loveless@gmail.com>
2020-12-12 01:26:05 +0100

    style(util: db): prepare a bit for publishing as a separate lib

--- actionless/util/db.lua
+++ actionless/util/db.lua
@@ -5,18 +5,22 @@ local pickle = require("actionless.util.pickle")
 
 
 local db = {
-  file_table = nil,
-  was_changed = false,
+  -- "public":
   auto_write_timeout = 60,
+  filename = awful_util.getdir("config") .. "/config/config.db",
+  --@TODO:
+  --filename = awful_util.getdir("config") .. "/local.db",
+
+  -- "private":
+  _file_table = nil,
+  _was_changed = false,
 }
 
-db.filename = awful_util.getdir("config") .. "/config/config.db"
-
-db.init = function()
-  if not db.file_table then
-    db.file_table = pickle.load(db.filename)
-    if not db.file_table then
-      db.file_table = {}
+db._init = function()
+  if not db._file_table then
+    db._file_table = pickle.load(db.filename)
+    if not db._file_table then
+      db._file_table = {}
       db.write()
     end
     gears_timer({
@@ -30,33 +34,33 @@ db.init = function()
 end
 
 db.write = function()
-  if db.was_changed then
-    pickle.save(db.file_table, db.filename)
-    db.was_changed = false
+  if db._was_changed then
+    pickle.save(db._file_table, db.filename)
+    db._was_changed = false
   end
 end
 
 db.get = function(key)
-  db.init()
-  return db.file_table[key]
+  db._init()
+  return db._file_table[key]
 end
 
 db.set = function(key, value)
-  db.init()
-  db.file_table[key] = value
-  db.was_changed = true
+  db._init()
+  db._file_table[key] = value
+  db._was_changed = true
 end
 
 db.get_or_set = function(key, fallback_value)
-    local value = db.get(key)
-    if not value then
-      value = fallback_value
-      db.set(key, value)
-    end
-    return value
+  local value = db.get(key)
+  if not value then
+    value = fallback_value
+    db.set(key, value)
+  end
+  return value
 end
 
-function db.update_child(key, child_key, child_value)
+db.update_child = function(key, child_key, child_value)
   local value = db.get(key)
   value[child_key] = child_value
   db.set(key, value)
