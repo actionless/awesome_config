f10304cd05246930f99226251d4e96a05960d90b
actionless <actionless.loveless@gmail.com>
2021-06-12 21:43:45 +0200

    refactor(tag_previewz): split stuff to functions a bit

--- actionless/tag_previewz.lua
+++ actionless/tag_previewz.lua
@@ -30,10 +30,10 @@ local function rounded_rect(radius)
 end
 
 
-local function create_client_box(
+local function create_box(
     client_geo, client_icon, client_name, geo, img, buttons,
-    icon_size, scale, margin,
-    client_opacity, client_bg, client_border_color, client_border_width, client_radius
+    client_bg, client_fg, client_opacity, client_border_color, client_border_width, client_radius,
+    settings
 )
     local img_box
     if img then
@@ -41,16 +41,16 @@ local function create_client_box(
           image = gears.surface.load(img),
           resize = true,
           opacity = client_opacity,
-          forced_height = math.floor(client_geo.height * scale),
-          forced_width = math.floor(client_geo.width * scale),
+          forced_height = math.floor(client_geo.height * settings.scale),
+          forced_width = math.floor(client_geo.width * settings.scale),
           widget = wibox.widget.imagebox
       }
     elseif client_icon then
       img_box = wibox.widget {
           image = gears.surface.load(client_icon),
           resize = true,
-          forced_height = icon_size,
-          forced_width = icon_size,
+          forced_height = settings.icon_size,
+          forced_width = settings.icon_size,
           widget = wibox.widget.imagebox,
           valign = "center",
       }
@@ -70,10 +70,10 @@ local function create_client_box(
                         )
                       ),
                       layout = wibox.layout.fixed.horizontal,
-                      spacing = margin * 2,
+                      spacing = settings.margin * 2,
                       fill_space = false,
                     },
-                    margins = margin * 3,
+                    margins = settings.margin * 3,
                     widget = wibox.container.margin
                 },
                 nil,
@@ -84,10 +84,10 @@ local function create_client_box(
             expand = "outside",
             widget = wibox.layout.align.vertical
         },
-        forced_height = math.floor(client_geo.height * scale),
-        forced_width = math.floor(client_geo.width * scale),
+        forced_height = math.floor(client_geo.height * settings.scale),
+        forced_width = math.floor(client_geo.width * settings.scale),
         bg = client_bg,
-        fg = client_border_color,
+        fg = client_fg,
         border_color = client_border_color,
         border_width = client_border_width,
         shape = rounded_rect(client_radius),
@@ -95,131 +95,135 @@ local function create_client_box(
     }
 
     client_box.point = {
-        x = math.floor((client_geo.x - geo.x) * scale),
-        y = math.floor((client_geo.y - geo.y) * scale)
+        x = math.floor((client_geo.x - geo.x) * settings.scale),
+        y = math.floor((client_geo.y - geo.y) * settings.scale)
     }
     client_box:buttons(buttons)
     return client_box
 end
 
 
-local function draw_widget(tag_preview_box, original_tag, tag_preview_image, scale,
-                           screen_radius, client_radius, client_opacity,
-                           client_bg, client_border_color, client_border_width,
-                           widget_bg, widget_border_color, widget_border_width,
-                           geo, margin,
-                           screen_bg,
-                           icon_size, default_client_icon,
-                           tag_opacity, tag_bg, tag_bg_focus, tag_border_color, tag_border_width, tag_radius
-                           )
+local function create_client_box(tag_preview_box, c, t, s, geo, settings)
+    local client_name = c.name
+    local client_icon = c.icon or settings.default_client_icon
+    local client_geo = {
+      height = c.height,
+      width = c.width,
+      x = c.x,
+      y = c.y
+    }
+
+    local buttons = awful.util.table.join({
+      awful.button({}, 1, function()
+        if not tag_preview_box.visible then return end
+        awesome.emit_signal(
+          "tag_previewz::visibility::toggle",
+          s, { visible = false }
+        )
+        t:view_only()
+        client.focus = c
+        c:raise()
+      end)
+    })
+
+    local img
+    if settings.tag_preview_image then
+        if c.prev_content or t.selected then
+        local content
+        if t.selected then
+            content = gears.surface(c.content)
+        else
+            content = gears.surface(c.prev_content)
+        end
+
+          local cr = cairo.Context(content)
+          local x, y, w, h = cr:clip_extents()
+          img = cairo.ImageSurface.create(
+              cairo.Format.ARGB32, w - x, h - y
+          )
+          cr = cairo.Context(img)
+          cr:set_source_surface(content, 0, 0)
+          cr.operator = cairo.Operator.SOURCE
+          cr:paint()
+      end
+    end
+
+    local client_box = create_box(
+        client_geo, client_icon, client_name, geo, img, buttons,
+        settings.client_bg, settings.client_fg, settings.client_opacity, settings.client_border_color,
+        settings.client_border_width, settings.client_radius,
+        settings
+    )
+    return client_box
+end
+
+
+local function create_tag_box(tag_preview_box, t, s, geo, settings)
+    local tag_buttons = awful.util.table.join({
+      awful.button({}, 1, function()
+        if not tag_preview_box.visible then return end
+        awesome.emit_signal(
+          "tag_previewz::visibility::toggle",
+          s, { visible = false }
+        )
+        t:view_only()
+      end)
+    })
+    local tag_geo = {
+      x = 10, y = 10, height = 250, width = 400,
+    }
+    local this_tag_bg = settings.tag_bg
+    local this_tag_fg = settings.tag_fg
+    for _, each_selected_tag in ipairs(s.selected_tags) do
+      if t == each_selected_tag then
+        this_tag_bg = settings.tag_bg_focus
+        this_tag_fg = settings.tag_fg_focus
+        break
+      end
+    end
+    local tag_box = create_box(
+        tag_geo, nil, t.name, geo, nil, tag_buttons,
+        this_tag_bg, this_tag_fg, settings.tag_opacity, settings.tag_border_color,
+        settings.tag_border_width, settings.tag_radius,
+        settings
+    )
+    return tag_box
+end
+
+
+local function draw_widget(tag_preview_box, s, geo, settings)
 
     local client_lists = {}
-    local s = original_tag.screen
     for _, t in ipairs(s.tags) do
-
       local client_list = wibox.layout.manual()
       table.insert(client_lists, client_list)
       client_list.forced_height = geo.height
       client_list.forced_width = geo.width
-
       for _, c in ipairs(t:clients()) do
-
         if not c.hidden and not c.minimized then
-            local client_name = c.name
-            local client_icon = c.icon or default_client_icon
-            local client_geo = {
-              height = c.height,
-              width = c.width,
-              x = c.x,
-              y = c.y
-            }
-
-            local buttons = awful.util.table.join({
-              awful.button({}, 1, function()
-                if not tag_preview_box.visible then return end
-                awesome.emit_signal(
-                  "tag_previewz::visibility::toggle",
-                  s, { visible = false }
-                )
-                t:view_only()
-                client.focus = c
-                c:raise()
-              end)
-            })
-
-            local img
-            if tag_preview_image then
-                if c.prev_content or t.selected then
-                local content
-                if t.selected then
-                    content = gears.surface(c.content)
-                else
-                    content = gears.surface(c.prev_content)
-                end
-
-                  local cr = cairo.Context(content)
-                  local x, y, w, h = cr:clip_extents()
-                  img = cairo.ImageSurface.create(
-                      cairo.Format.ARGB32, w - x, h - y
-                  )
-                  cr = cairo.Context(img)
-                  cr:set_source_surface(content, 0, 0)
-                  cr.operator = cairo.Operator.SOURCE
-                  cr:paint()
-              end
-            end
-
-            local client_box = create_client_box(
-                client_geo, client_icon, client_name, geo, img, buttons,
-                icon_size, scale, margin,
-                client_opacity, client_bg, client_border_color, client_border_width, client_radius
-            )
-            client_list:add(client_box)
+            client_list:add(create_client_box(
+              tag_preview_box, c, t, s, geo, settings
+            ))
         end
       end
-
-      local tag_buttons = awful.util.table.join({
-        awful.button({}, 1, function()
-          if not tag_preview_box.visible then return end
-          awesome.emit_signal(
-            "tag_previewz::visibility::toggle",
-            s, { visible = false }
-          )
-          t:view_only()
-        end)
-      })
-      local tag_geo = {
-        x = 10, y = 10, height = 250, width = 400,
-      }
-      local this_tag_bg = tag_bg
-      for _, each_selected_tag in ipairs(s.selected_tags) do
-        if t == each_selected_tag then
-          this_tag_bg = tag_bg_focus
-          break
-        end
-      end
-      local tag_box = create_client_box(
-          tag_geo, nil, t.name, geo, nil, tag_buttons,
-          icon_size, scale, margin,
-          tag_opacity, this_tag_bg, tag_border_color, tag_border_width, tag_radius
-      )
-      client_list:add(tag_box)
-
+      client_list:add(create_tag_box(
+        tag_preview_box, t, s, geo, settings
+      ))
     end
 
     local all_widths = 0
     local previews_v = wibox.layout.fixed.vertical()
     previews_v.fill_space = false
-    previews_v.spacing = margin * 2
+    previews_v.spacing = settings.margin * 2
     local previews_h = wibox.layout.fixed.horizontal()
     previews_h.fill_space = false
-    previews_h.spacing = margin * 2
+    previews_h.spacing = settings.margin * 2
     previews_v:add(previews_h)
     for tag_idx, client_list in ipairs(client_lists) do
-      if geo.width * scale * tag_idx > (geo.width + all_widths) then
+      if geo.width * settings.scale * tag_idx > (geo.width + all_widths) then
         previews_h = wibox.layout.fixed.horizontal()
         previews_h.fill_space = false
-        previews_h.spacing = margin * 2
+        previews_h.spacing = settings.margin * 2
         previews_v:add(previews_h)
         all_widths = all_widths + geo.width
       end
@@ -230,78 +234,91 @@ local function draw_widget(tag_preview_box, original_tag, tag_preview_image, sca
                       {
                         {
                             client_list,
-                            bg = screen_bg,
+                            bg = settings.screen_bg,
                             widget = wibox.container.background
                         },
                         layout = wibox.container.constraint,
-                        height = geo.height * scale,
-                        width = geo.width * scale,
+                        height = geo.height * settings.scale,
+                        width = geo.width * settings.scale,
                         strategy = "exact",
                       },
                       layout = wibox.layout.align.horizontal
                   },
                   layout = wibox.layout.align.vertical
               },
-              margins = margin,
+              margins = settings.margin,
               widget = wibox.container.margin
           },
-          bg = widget_bg,
-          border_width = widget_border_width,
-          border_color = widget_border_color,
-          shape = rounded_rect(screen_radius),
+          bg = settings.widget_bg,
+          border_width = settings.widget_border_width,
+          border_color = settings.widget_border_color,
+          shape = rounded_rect(settings.screen_radius),
           widget = wibox.container.background
       })
     end
     tag_preview_box:set_widget(previews_v)
 end
 
+
+local function get_settings(opts)
+  local settings = {
+    tag_preview_image = false,
+    --tag_preview_image = true,
+    --widget_x = dpi(20),
+    --widget_y = dpi(20),
+    widget_x = dpi(0),
+    --widget_y = beautiful.panel_height or dpi(0),
+    widget_y = dpi(0),
+    margin = beautiful.tag_preview_widget_margin or dpi(1),
+    screen_radius = (
+      beautiful.tag_preview_widget_border_radius or
+      (beautiful.client_border_radius or dpi(0))*2.7
+    ),
+    client_radius = beautiful.tag_preview_client_border_radius or beautiful.client_border_radius or dpi(0),
+    client_opacity = beautiful.tag_preview_client_opacity or 0.5,  -- ???
+    client_bg = beautiful.tag_preview_client_bg or "#60006088",
+    client_border_color = beautiful.tag_preview_client_border_color or
+                              "#ffffff88",
+    client_border_width = beautiful.tag_preview_client_border_width or
+                              dpi(3),
+    widget_bg = beautiful.tag_preview_widget_bg or "#00000013",  -- ???
+    widget_border_color = beautiful.tag_preview_widget_border_color or "#ffffff22",
+    widget_border_width = beautiful.tag_preview_widget_border_width or dpi(0),
+
+    scale = 0.1963,
+    --scale = 0.2,
+    work_area = false,
+    padding = false,
+
+    icon_size = 32 or dpi(24),
+    default_client_icon = get_icon('apps', 'terminal'),
+
+    screen_bg = beautiful.tag_preview_screen_bg or "#60600023",
+    tag_bg = beautiful.tag_preview_tag_bg or "#00606088",
+    tag_bg_focus = beautiful.tag_preview_tag_bg_focus or beautiful.taglist_bg_focus or "#00606088",
+  }
+  settings.client_fg = settings.client_border_color
+
+  settings.tag_opacity = settings.client_opacity
+  settings.tag_radius = settings.client_radius
+  settings.tag_border_width = settings.client_border_width
+  settings.tag_border_color = settings.client_border_color
+  settings.tag_fg = settings.tag_border_color
+
+  if opts then
+    settings.tag_preview_image = opts.show_client_content or settings.tag_preview_image
+    settings.widget_x = opts.x or settings.widget_x
+    settings.widget_y = opts.y or settings.widget_y
+    settings.scale = opts.scale or settings.scale
+    settings.work_area = opts.honor_workarea or settings.work_area
+    settings.padding = opts.honor_padding or settings.padding
+  end
+
+  return settings
+end
+
+
 local enable = function(opts)
-    local tag_preview_image = false
-    --local tag_preview_image = true
-    --local widget_x = dpi(20)
-    --local widget_y = dpi(20)
-    local widget_x = dpi(0)
-    --local widget_y = beautiful.panel_height or dpi(0)
-    local widget_y = dpi(0)
-    local margin = beautiful.tag_preview_widget_margin or dpi(1)
-    local screen_radius = beautiful.tag_preview_widget_border_radius or (beautiful.client_border_radius or dpi(0))*2.7
-    local client_radius = beautiful.tag_preview_client_border_radius or beautiful.client_border_radius or dpi(0)
-    local client_opacity = beautiful.tag_preview_client_opacity or 0.5
-    local client_bg = beautiful.tag_preview_client_bg or "#60006088"
-    local client_border_color = beautiful.tag_preview_client_border_color or
-                                    "#ffffff88"
-    local client_border_width = beautiful.tag_preview_client_border_width or
-                                    dpi(3)
-    local widget_bg = beautiful.tag_preview_widget_bg or "#00000013"  -- ???
-    local widget_border_color = beautiful.tag_preview_widget_border_color or
-                                    "#ffffff22"
-    local widget_border_width = beautiful.tag_preview_widget_border_width or
-                                    dpi(0)
-
-    local screen_bg = beautiful.tag_preview_screen_bg or "#60600023"
-    local tag_bg = beautiful.tag_preview_tag_bg or "#00606088"
-    local tag_bg_focus = beautiful.tag_preview_tag_bg_focus or beautiful.taglist_bg_focus or "#00606088"
-    local tag_opacity = client_opacity
-    local tag_border_color = client_border_color
-    local tag_border_width = client_border_width
-    local tag_radius = client_radius
-
-    local scale = 0.1963
-    --local scale = 0.2
-    local work_area = false
-    local padding = false
-
-    local icon_size = 32 or dpi(24)
-    local default_client_icon = get_icon('apps', 'terminal')
-
-    if opts then
-        tag_preview_image = opts.show_client_content or tag_preview_image
-        widget_x = opts.x or widget_x
-        widget_y = opts.y or widget_y
-        scale = opts.scale or scale
-        work_area = opts.honor_workarea or work_area
-        padding = opts.honor_padding or padding
-    end
 
     local tag_preview_box = wibox({
         visible = false,
@@ -310,7 +327,9 @@ local enable = function(opts)
         bg = "#00000000"
     })
 
-    if tag_preview_image then
+    local settings = get_settings(opts)
+
+    if settings.tag_preview_image then
       tag.connect_signal("property::selected", function(t)
           for _, c in ipairs(t:clients()) do
               c.prev_content = gears.surface.duplicate_surface(c.content)
@@ -320,20 +339,15 @@ local enable = function(opts)
 
     awesome.connect_signal("tag_previewz::update", function(t)
         local geo = t.screen:get_bounding_geometry{
-            honor_padding = padding,
-            honor_workarea = work_area
+            honor_padding = settings.padding,
+            honor_workarea = settings.work_area
         }
 
         tag_preview_box.width = geo.width
         tag_preview_box.height = geo.height
 
         draw_widget(
-          tag_preview_box, t, tag_preview_image, scale, screen_radius,
-          client_radius, client_opacity, client_bg,
-          client_border_color, client_border_width, widget_bg,
-          widget_border_color, widget_border_width, geo, margin,
-          screen_bg, icon_size, default_client_icon,
-          tag_opacity, tag_bg, tag_bg_focus, tag_border_color, tag_border_width, tag_radius
+          tag_preview_box, t.screen, geo, settings
         )
     end)
 
@@ -345,8 +359,8 @@ local enable = function(opts)
       end
       local target_state = not tag_preview_box.visible
       if args.visible ~= nil then target_state = args.visible end
-      tag_preview_box.x = s.geometry.x + widget_x
-      tag_preview_box.y = s.geometry.y + widget_y
+      tag_preview_box.x = s.geometry.x + settings.widget_x
+      tag_preview_box.y = s.geometry.y + settings.widget_y
       tag_preview_box.visible = target_state
     end)
 end
