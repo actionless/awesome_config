019544cf8fe65faf2099f56f89546d1a10507005
actionless <actionless.loveless@gmail.com>
2021-06-12 22:11:23 +0200

    chore(.): add init.lua

---
+++ init.lua
@@ -0,0 +1,6 @@
+local actionless_awesome_config = {
+  actionless = require("actionless_awesome_config.actionless"),
+  config = require("actionless_awesome_config.config"),
+  hotkeys = require("actionless_awesome_config.hotkeys"),
+}
+return actionless_awesome_config
