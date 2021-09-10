f8ab9cba534c89be8f1051666ee3d9f489d74068
actionless <actionless.loveless@gmail.com>
2021-09-10 14:44:49 +0200

    feat(config: menu): cache menu content once to avoid loading the icons again

--- config/menu.lua
+++ config/menu.lua
@@ -20,195 +20,204 @@ local menus = {}
 function menus.init(context)
   local term = awesome_menubar.utils.terminal .. " -e "
 
-  local myawesomemenu = {
-    { "hotkeys", function()
-        return false, hotkeys_popup.show_help
-      end, get_icon('devices', 'keyboard')
-    },
-    { "manual page", term .. "man awesome",
-      get_icon('actions', 'help-contents')
-    },
-    { "edit config", context.cmds.editor_cmd .. " " .. awesome.conffile,
-      get_icon('actions', 'document-properties')
-    },
-    { "reload", awesome.restart,
-      get_icon('actions', 'view-refresh')
-    },
-    { "quit (hard reload)", function()
-        awesome.quit()
-      end, get_icon('actions', 'application-exit')
-    },
-    { "quit2 (toggle argb)", function()
-        awesome.quit(2)
-      end, get_icon('actions', 'format-text-italic')
-    },
-    { "quit3 (openbox)", function()
-        awesome.quit(3)
-      end, get_icon('apps', 'openbox')
-    },
-    { "quit9 (to DM)", function()
-        awesome.quit(9)
-      end, get_icon('actions', 'window-close')
-    },
-  }
-
-  local shutdown_menu = {
-    { "hibernate",
-      "xscreensaver-command -lock ; sudo systemctl hibernate",
-      get_icon("apps", "system-hibernate") },
-    -- Without X Session Manager:
-    { "reboot", function()
-        shutdown.kill_everybody(function()
-          awful_spawn("reboot")
-        end)
-      end, get_icon('apps', 'system-restart')
-    },
-    { "poweroff", function()
-        shutdown.kill_everybody(function()
-          awful_spawn("poweroff")
-        end)
-      end, get_icon('apps', 'system-shutdown')
-    },
-    { "force shutdown", shutdown.skip_kill,
-      get_icon('actions', 'edit-redo')
-    },
-    { "cancel shutdown", shutdown.cancel_kill,
-      get_icon('actions', 'edit-undo')
-    },
-    -- With X Session Manager:
-    --{ "logout", session_logout, get_icon('actions', 'system-log-out') },
-    --{ "reboot", session_reboot, get_icon('actions', 'view-refresh') },
-    --{ "poweroff", session_poweroff, get_icon('actions', 'system-shutdown') },
-  }
-
-  local function app(display_name, name, icon_name, fallback_icon_name)
-    name = name or display_name
-    icon_name = icon_name or name
-    return {
-      display_name, name,
-      awesome_menubar.utils.lookup_icon(icon_name) or (
-        fallback_icon_name and awesome_menubar.utils.lookup_icon(fallback_icon_name)
-      )
+  local _cached_menu_content
+  local function get_menu_content()
+    if _cached_menu_content then
+      return _cached_menu_content
+    end
+    local myawesomemenu = {
+      { "hotkeys", function()
+          return false, hotkeys_popup.show_help
+        end, get_icon('devices', 'keyboard')
+      },
+      { "manual page", term .. "man awesome",
+        get_icon('actions', 'help-contents')
+      },
+      { "edit config", context.cmds.editor_cmd .. " " .. awesome.conffile,
+        get_icon('actions', 'document-properties')
+      },
+      { "reload", awesome.restart,
+        get_icon('actions', 'view-refresh')
+      },
+      { "quit (hard reload)", function()
+          awesome.quit()
+        end, get_icon('actions', 'application-exit')
+      },
+      { "quit2 (toggle argb)", function()
+          awesome.quit(2)
+        end, get_icon('actions', 'format-text-italic')
+      },
+      { "quit3 (openbox)", function()
+          awesome.quit(3)
+        end, get_icon('apps', 'openbox')
+      },
+      { "quit9 (to DM)", function()
+          awesome.quit(9)
+        end, get_icon('actions', 'window-close')
+      },
     }
-  end
 
-  local function category(display_name, content, icon_name, fallback_icon_name)
-    return {
-      display_name, content,
-      get_icon('categories', 'applications-'..icon_name) or (
-        fallback_icon_name and awesome_menubar.utils.lookup_icon(fallback_icon_name)
-      )
+    local shutdown_menu = {
+      { "hibernate",
+        "xscreensaver-command -lock ; sudo systemctl hibernate",
+        get_icon("apps", "system-hibernate") },
+      -- Without X Session Manager:
+      { "reboot", function()
+          shutdown.kill_everybody(function()
+            awful_spawn("reboot")
+          end)
+        end, get_icon('apps', 'system-restart')
+      },
+      { "poweroff", function()
+          shutdown.kill_everybody(function()
+            awful_spawn("poweroff")
+          end)
+        end, get_icon('apps', 'system-shutdown')
+      },
+      { "force shutdown", shutdown.skip_kill,
+        get_icon('actions', 'edit-redo')
+      },
+      { "cancel shutdown", shutdown.cancel_kill,
+        get_icon('actions', 'edit-undo')
+      },
+      -- With X Session Manager:
+      --{ "logout", session_logout, get_icon('actions', 'system-log-out') },
+      --{ "reboot", session_reboot, get_icon('actions', 'view-refresh') },
+      --{ "poweroff", session_poweroff, get_icon('actions', 'system-shutdown') },
     }
+
+    local function app(display_name, name, icon_name, fallback_icon_name)
+      name = name or display_name
+      icon_name = icon_name or name
+      return {
+        display_name, name,
+        awesome_menubar.utils.lookup_icon(icon_name) or (
+          fallback_icon_name and awesome_menubar.utils.lookup_icon(fallback_icon_name)
+        )
+      }
+    end
+
+    local function category(display_name, content, icon_name, fallback_icon_name)
+      return {
+        display_name, content,
+        get_icon('categories', 'applications-'..icon_name) or (
+          fallback_icon_name and awesome_menubar.utils.lookup_icon(fallback_icon_name)
+        )
+      }
+    end
+
+    local applications_menu = {
+
+      category("Development", {
+        app("Git-Cola", "git-cola"),
+        app("Meld", "meld"),
+        app("PTIPython", term .. "ptipython", "ipython"),
+      }, "development"),
+
+      category("Graphics", {
+        app("Drawing", "drawing", "com.github.maoschanz.drawing"),
+        app("GIMP", "gimp"),
+        app("Gpick", "gpick"),
+        app("Nomacs", "nomacs"),
+        app("Viewnior", "viewnior"),
+      }, "graphics"),
+
+      category("Markdown", {
+        app("abricotine"),  -- best inline
+        app("ghostwriter"),  -- good inline + preview
+        app("marker", "marker", "com.github.fabiocolacio.marker"),  -- classic + preview (GTK)
+        app("retext"),  -- classic + preview (Qt)
+      }, "education-language"),
+
+      category("Media players", {
+        app("Clementine", "clementine"),
+        app("Goodvibes", "goodvibes", "io.gitlab.Goodvibes"),
+        app("MPV", "mpv"),
+        --app("Shortwave", "shortwave", "de.haeckerfelix.Shortwave"), -- nice but buggy
+      }, "multimedia"),
+
+      category("Productivity", {
+        app("Go For It!", "com.github.jmoerman.go-for-it"),
+        --app("GNOME To Do", "gnome-todo"),
+        app("QOwnNotes", "QOwnNotes"),
+        --app("vnote", "vnote"),  -- replace to qOwnNotes or mindforger?
+      }, "office"),
+
+      category("Settings", {
+        app("dconf Editor", "dconf-editor"),
+      }, "system"),
+
+      category("Terminals", {
+        app("st", "st", 'terminal'),
+        app("tmux in xst", "xst-tmux", 'terminator'),
+        app("URxvt", "urxvt"),
+        app("xst", "xst", 'terminal'),
+        app("XTerm", "xterm"),
+      }, "terminal", "terminal"),
+
+      category("Text", {
+        --app("Geany", "geany"),
+        app("Oni2", "Oni2", "Onivim2"),
+        app("xed", "xed", "accessories-text-editor"),
+      }, "office"),
+
+      category("Video Editing", {
+        app("Flowblade", "flowblade"),
+        app("Kdenlive", "kdenlive"),
+        app("Shotcut", "shotcut"),
+      }, "video", "multimedia-video-player"),
+
+    }
+
+    local menu_content = {
+      {
+        "freedesktop loading...", nil,
+        get_icon('status', 'image-loading')
+      },
+      {
+        "open terminal", "bash -c 'xst-tmux || "..awesome_menubar.utils.terminal.."'",
+        get_icon('apps', 'terminal')
+      },
+      --{ "kill compositor", "killall compton" },
+      --{ "start compositor", context.cmds.compositor },
+      {
+        "wlppr",
+        {{
+          "save", wlppr.save,
+          get_icon('actions', 'filesave')
+        }, {
+          "save to best", wlppr.save_best,
+          get_icon('status', 'starred')
+        }, {
+          "dump", wlppr.dump,
+          get_icon('status', 'user-trash-full')
+        }},
+        get_icon('apps', 'preferences-desktop-wallpaper')
+      },
+      {
+        "jack",
+        {{
+          "start", os.getenv("HOME").."/scripts/jack_start.sh",
+          get_icon('devices', 'audio-speakers')
+        }, {
+          "stop", os.getenv("HOME").."/scripts/jack_stop.sh",
+          get_icon('actions', 'stop')
+        }},
+        --get_icon('devices', 'audio-input-microphone')
+        get_icon('apps', 'audio-player')
+      },
+      { "applications", applications_menu, get_icon('apps', 'menu-editor') },
+      { "shutdown", shutdown_menu, get_icon('apps', 'system-shutdown') },
+      { "awesome", myawesomemenu, beautiful.awesome_icon },
+      --{ "mpv-xsel",
+      --  function() awful_spawn.with_shell('mpv "$(xsel -b)"') end,
+      --  get_icon('apps', 'mpv')
+      --},
+    }
+
+    _cached_menu_content = menu_content
+    return menu_content
   end
-
-  local applications_menu = {
-
-    category("Development", {
-      app("Git-Cola", "git-cola"),
-      app("Meld", "meld"),
-      app("PTIPython", term .. "ptipython", "ipython"),
-    }, "development"),
-
-    category("Graphics", {
-      app("Drawing", "drawing", "com.github.maoschanz.drawing"),
-      app("GIMP", "gimp"),
-      app("Gpick", "gpick"),
-      app("Nomacs", "nomacs"),
-      app("Viewnior", "viewnior"),
-    }, "graphics"),
-
-    category("Markdown", {
-      app("abricotine"),  -- best inline
-      app("ghostwriter"),  -- good inline + preview
-      app("marker", "marker", "com.github.fabiocolacio.marker"),  -- classic + preview (GTK)
-      app("retext"),  -- classic + preview (Qt)
-    }, "education-language"),
-
-    category("Media players", {
-      app("Clementine", "clementine"),
-      app("Goodvibes", "goodvibes", "io.gitlab.Goodvibes"),
-      app("MPV", "mpv"),
-      --app("Shortwave", "shortwave", "de.haeckerfelix.Shortwave"), -- nice but buggy
-    }, "multimedia"),
-
-    category("Productivity", {
-      app("Go For It!", "com.github.jmoerman.go-for-it"),
-      --app("GNOME To Do", "gnome-todo"),
-      app("QOwnNotes", "QOwnNotes"),
-      --app("vnote", "vnote"),  -- replace to qOwnNotes or mindforger?
-    }, "office"),
-
-    category("Settings", {
-      app("dconf Editor", "dconf-editor"),
-    }, "system"),
-
-    category("Terminals", {
-      app("st", "st", 'terminal'),
-      app("tmux in xst", "xst-tmux", 'terminator'),
-      app("URxvt", "urxvt"),
-      app("xst", "xst", 'terminal'),
-      app("XTerm", "xterm"),
-    }, "terminal", "terminal"),
-
-    category("Text", {
-      --app("Geany", "geany"),
-      app("Oni2", "Oni2", "Onivim2"),
-      app("xed", "xed", "accessories-text-editor"),
-    }, "office"),
-
-    category("Video Editing", {
-      app("Flowblade", "flowblade"),
-      app("Kdenlive", "kdenlive"),
-      app("Shotcut", "shotcut"),
-    }, "video", "multimedia-video-player"),
-
-  }
-
-  local menu_content = {
-    {
-      "freedesktop loading...", nil,
-      get_icon('status', 'image-loading')
-    },
-    {
-      "open terminal", "bash -c 'xst-tmux || "..awesome_menubar.utils.terminal.."'",
-      get_icon('apps', 'terminal')
-    },
-    --{ "kill compositor", "killall compton" },
-    --{ "start compositor", context.cmds.compositor },
-    {
-      "wlppr",
-      {{
-        "save", wlppr.save,
-        get_icon('actions', 'filesave')
-      }, {
-        "save to best", wlppr.save_best,
-        get_icon('status', 'starred')
-      }, {
-        "dump", wlppr.dump,
-        get_icon('status', 'user-trash-full')
-      }},
-      get_icon('apps', 'preferences-desktop-wallpaper')
-    },
-    {
-      "jack",
-      {{
-        "start", os.getenv("HOME").."/scripts/jack_start.sh",
-        get_icon('devices', 'audio-speakers')
-      }, {
-        "stop", os.getenv("HOME").."/scripts/jack_stop.sh",
-        get_icon('actions', 'stop')
-      }},
-      --get_icon('devices', 'audio-input-microphone')
-      get_icon('apps', 'audio-player')
-    },
-    { "applications", applications_menu, get_icon('apps', 'menu-editor') },
-    { "shutdown", shutdown_menu, get_icon('apps', 'system-shutdown') },
-    { "awesome", myawesomemenu, beautiful.awesome_icon },
-    --{ "mpv-xsel",
-    --  function() awful_spawn.with_shell('mpv "$(xsel -b)"') end,
-    --  get_icon('apps', 'mpv')
-    --},
-  }
 
 
   function context.menu.mainmenu_show(nomouse)
@@ -221,7 +230,7 @@ function menus.init(context)
       show_menu()
     else
       context.menu.mainmenu = awful_menu({
-        items = menu_content,
+        items = get_menu_content(),
       })
       show_menu()
       menugen.build_menu(function(menulist)
