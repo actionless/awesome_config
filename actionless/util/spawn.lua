d3b9a57ea0431c0f4f73ca3d2ac17f11cc89dff1
actionless <actionless.loveless@gmail.com>
2021-11-06 09:17:04 +0100

    refactor(util: spawn: run_once): use line callback to the get the status of the commands instead of inlining bash

--- actionless/util/spawn.lua
+++ actionless/util/spawn.lua
@@ -1,6 +1,9 @@
 local awful_spawn = require("awful.spawn")
+local g_string = require("gears.string")
 
-local log = require("actionless.util.debug").log
+local h_string = require("actionless.util.string")
+local h_table = require("actionless.util.table")
+local log = require("actionless.util.debug").get_decorated_logger(':RUN_ONCE:')
 
 
 local spawn = {}
@@ -9,10 +12,31 @@ function spawn.run_once(cmd)
   if type(cmd) == "table" then
     cmd = table.concat(cmd, " ")
   end
-  log('::RUN_ONCE: Going to start "'..cmd..'"...')
-  awful_spawn.with_shell(
-    'ps x | grep -v grep | grep "' .. cmd ..
-    '" || ('..cmd..' & echo "Started \\"'..cmd..'\\" with PID $!")'
+  log('Going to start "'..cmd..'"...')
+  awful_spawn.easy_async_with_shell(
+    'ps x | grep -v grep | grep "' .. cmd .. '"',
+    function(output, err_output)
+      if err_output ~= '' then
+        log('ERROR: '..err_output)
+        return
+      end
+      if output ~= '' then
+        local pid, _tty, _stat, _time, _command = h_table.unpack(
+            g_string.split(
+              h_string.lstrip(
+                h_string.rstrip(output, '\n'),
+                ' '
+              ),
+              ' '
+            )
+          )
+        log('"'..cmd..'" is already running with PID '..pid)
+      else
+        log('Starting "'..cmd..'"...')
+        local pid = awful_spawn.spawn(cmd)
+        log('Started "'..cmd..'" with PID '..tostring(pid))
+      end
+    end
   )
 end
 
