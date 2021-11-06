1c8037f07a5369bff7f7cc89c66af612bbb07942
actionless <actionless.loveless@gmail.com>
2021-11-06 09:15:47 +0100

    feat(util: debug): add `naughty_log_eternal`/`nloge`

--- actionless/util/debug.lua
+++ actionless/util/debug.lua
@@ -25,15 +25,30 @@ function debug_module.log(...)
   eprint('[AWESOME_CONFIG] '..my_inspect(...))
 end
 
-function debug_module.naughty_log(object)
-  local formatted = my_inspect(object)
-  eprint("nlog: " .. formatted)
-  naughty.notify{
+function debug_module._naughty_log(formatted, naughty_args)
+  naughty_args = naughty_args or {}
+  local default_naughty_args = {
     title="DEBUG",
     text=formatted,
     timeout=60,
   }
+  for k, v in pairs(naughty_args) do
+    default_naughty_args[k] = v
+  end
+  eprint("nlog: " .. formatted)
+  naughty.notify(default_naughty_args)
 end
+
+function debug_module.naughty_log(...)
+  local formatted = my_inspect(...)
+  debug_module._naughty_log(formatted)
+end
+
+function debug_module.naughty_log_eternal(...)
+  local formatted = my_inspect(...)
+  debug_module._naughty_log(formatted, {timeout=0})
+end
+
 
 function debug_module.get_decorated_logger(module_name)
   return function(...)
@@ -42,5 +57,6 @@ function debug_module.get_decorated_logger(module_name)
 end
 
 debug_module.nlog = debug_module.naughty_log
+debug_module.nloge = debug_module.naughty_log_eternal
 
 return debug_module
